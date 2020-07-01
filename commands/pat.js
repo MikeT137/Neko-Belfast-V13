2e649a307cfbbf8c57770e17f5f45e1fc2f18ebe
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-01 13:30:02 +0300

    Create pat.js

---
+++ commands/pat.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name:'pat',
+    description: "Pats a user",
+    execute(message, argument){
+        let member = message.mentions.members.first()
+
+        if(argument[2] == `${member}`){
+            message.channel.send(`${message.author} gave ${member} a pat!`);
+            message.channel.send("https://media.giphy.com/media/osYdfUptPqV0s/giphy.gif");
+        }
+    }
+}
