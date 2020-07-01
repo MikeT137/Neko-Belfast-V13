e4c0d76ee0b761882ee297fb1476d1d4dbe0aa2d
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-01 13:08:46 +0300

    Create hug.js

---
+++ commands/hug.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name:'hug',
+    description: "Hugs a user",
+    execute(message, argument){
+        let member = message.mentions.members.first()
+
+        if(!argument[3]){
+            message.channel.send(`${message.author} gave ${member} a hug!`, {
+            file: "https://media.giphy.com/media/l2QDM9Jnim1YVILXa/giphy.gif"
+            });
+        }
+    }
+}
