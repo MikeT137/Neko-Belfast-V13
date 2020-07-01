4e8c037732a65c1de8abf0ec2fdffe00e55ae5b8
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-01 13:51:55 +0300

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -2,11 +2,11 @@ module.exports = {
     name:'hug',
     description: "Hugs a user",
     execute(message, argument){
-        let member = message.mentions.members.first()
-
-        if(argument[2] == `${member}`){
-            message.channel.send(`${message.author} gave ${member} a hug!`);
-            message.channel.send("https://media.giphy.com/media/l2QDM9Jnim1YVILXa/giphy.gif");
+        if(message.mentions.members.size == 1) {
+            let member = message.mentions.members.first()
+            message.channel.send(`${message.author} gave ${member} a hug!`, {
+                file: "https://media.giphy.com/media/l2QDM9Jnim1YVILXa/giphy.gif"
+            });
         }
     }
 }
