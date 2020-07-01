215e14dca3abe1bd308316720aa0e92572bb62bb
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-01 13:57:21 +0300

    Update pat.js

--- commands/pat.js
+++ commands/pat.js
@@ -3,11 +3,9 @@ module.exports = {
     description: "Pats a user",
     execute(message, argument){
         if(message.mentions.members.size == 1) {
-        let member = message.mentions.members.first()
-
-        if(argument[2] == `${member}`){
-            message.channel.send(`${message.author} gave ${member} a pat!`);
-            message.channel.send('https://media.giphy.com/media/osYdfUptPqV0s/giphy.gif');
+            let member = message.mentions.members.first()
+            message.channel.send(`${message.author} pats ${member}! How cute`);
+            message.channel.send("https://media.giphy.com/media/osYdfUptPqV0s/giphy.gif");
         }
     }
-}}
+}
