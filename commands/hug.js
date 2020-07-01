dada00e69e1bee68bba7445dad366c3343119ab2
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-01 13:37:09 +0300

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -6,7 +6,7 @@ module.exports = {
 
         if(argument[2] == `${member}`){
             message.channel.send(`${message.author} gave ${member} a hug!`);
-            message.channel.send(`https://media.giphy.com/media/l2QDM9Jnim1YVILXa/giphy.gif`);
+            message.channel.send('https://media.giphy.com/media/l2QDM9Jnim1YVILXa/giphy.gif');
         }
     }
 }
