d9de823472b63a1fa2904a6007a2732f3ef96cff
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-01 22:37:21 +0300

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -3,9 +3,18 @@ module.exports = {
     description: "Kisses a user",
     execute(message, argument){
         if(message.mentions.members.size == 1) {
-            let member = message.mentions.members.first()
-            message.channel.send(`${message.author} kisses ${member} !:blush:`);
-            message.channel.send("https://media.giphy.com/media/MQVpBqASxSlFu/giphy.gif");
+            const random_kiss = [
+                'https://media.giphy.com/media/MQVpBqASxSlFu/giphy.gif',
+                'https://media.giphy.com/media/G3va31oEEnIkM/giphy.gif',
+                'https://media.giphy.com/media/hnNyVPIXgLdle/giphy.gif',
+                'https://media.giphy.com/media/11rWoZNpAKw8w/giphy.gif',
+                'https://media.giphy.com/media/jR22gdcPiOLaE/giphy.gif',
+                'https://media.giphy.com/media/flmwfIpFVrSKI/giphy.gif',
+            ]
+            if(message.mentions.members.size == 1) {
+                let member = message.mentions.members.first()
+                message.channel.send(`${message.author} kisses ${member} :flushed:`);
+                message.channel.send(random_kiss[Math.floor(Math.random() * random_kiss.length)]);
         }
     }
 }
