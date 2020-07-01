e29dfe9a8784404d714a06412ccc5009f3138510
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-01 13:57:16 +0300

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(message, argument){
         if(message.mentions.members.size == 1) {
             let member = message.mentions.members.first()
-            message.channel.send(`${message.author} gave ${member} a hug!`);
+            message.channel.send(`${message.author} hugs ${member} !`);
             message.channel.send("https://media.giphy.com/media/l2QDM9Jnim1YVILXa/giphy.gif");
         }
     }
