41bd300374327353ad8f5b7aa1e9c2cc6947c503
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-01 22:53:27 +0300

    Update snuggle.js

--- commands/snuggle.js
+++ commands/snuggle.js
@@ -2,10 +2,20 @@ module.exports = {
     name:'snuggle',
     description: "Snuggles a user",
     execute(message, argument){
+        const random_snuggle = [
+            'https://media.giphy.com/media/C4gbG94zAjyYE/giphy.gif',
+            'https://media.giphy.com/media/BXrwTdoho6hkQ/giphy.gif',
+            'https://media.giphy.com/media/eMpDBxxTzKety/giphy.gif',
+            'https://media.giphy.com/media/GMFUrC8E8aWoo/giphy.gif',
+            'https://media.giphy.com/media/3bqtLDeiDtwhq/giphy.gif',
+            'https://media.giphy.com/media/nUz7d1sUppGta/giphy.gif',
+            'https://media.giphy.com/media/EGauSkKQZuXxS/giphy.gif',
+            'https://media.giphy.com/media/PHZ7v9tfQu0o0/giphy.gif',
+        ]
         if(message.mentions.members.size == 1) {
             let member = message.mentions.members.first()
-            message.channel.send(`${message.author} snuggles ${member} !So adorable`);
-            message.channel.send("https://media.giphy.com/media/C4gbG94zAjyYE/giphy.gif");
+            message.channel.send(`${message.author} snuggles ${member}...so adorable`);
+            message.channel.send(random_snuggle[Math.floor(Math.random() * random_snuggle.length)]);
         }
     }
 }
