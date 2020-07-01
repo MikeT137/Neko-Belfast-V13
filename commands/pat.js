4daee30926ad9f19557ab79688382f8935f5895d
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-01 22:47:10 +0300

    Update pat.js

--- commands/pat.js
+++ commands/pat.js
@@ -2,10 +2,17 @@ module.exports = {
     name:'pat',
     description: "Pats a user",
     execute(message, argument){
+        const random_pat = [
+            'https://media.giphy.com/media/osYdfUptPqV0s/giphy.gif',
+            'https://media.giphy.com/media/L2z7dnOduqEow/giphy.gif',
+            'https://media.giphy.com/media/4HP0ddZnNVvKU/giphy.gif',
+            'https://media.giphy.com/media/109ltuoSQT212w/giphy.gif',
+            'https://media.giphy.com/media/ye7OTQgwmVuVy/giphy.gif',
+        ]
         if(message.mentions.members.size == 1) {
             let member = message.mentions.members.first()
-            message.channel.send(`${message.author} pats ${member}! How cute`);
-            message.channel.send("https://media.giphy.com/media/osYdfUptPqV0s/giphy.gif");
+            message.channel.send(`${message.author} pats ${member}...so cute`);
+            message.channel.send(random_pat[Math.floor(Math.random() * random_pat.length)]);
         }
     }
 }
