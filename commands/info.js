ef14dbd82d757419991b610ca2998d8f1f144570
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-01 14:43:32 +0300

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -15,7 +15,6 @@ module.exports = {
             .addField('Birthday', birthday)
             .addField('Creator', creator + ':flag_td:')
             .setColor(0x4AEFBA)
-            .setThumbnail('https://lh3.googleusercontent.com/proxy/HOezEcBPsOMAXMlF5J9hVWBTLE6rhQhns8Hrz1GB2cWI6HLYoqhETWFdfNYik68lz3P00HBvYpKVkTrPJinTOJ_2K66d9bnJ6_NRa14oUVjRtAAcRi7v6r7xNzdC811ORkR3lyco40h63TJ4B9FwdjnxEAXqS-x_-ujZm7QVhkJ3nEYbcKQOcY7Gn4YW90lYXWC33DKxFsqXN07fZz0')
             message.channel.send(embed);
         }
     }
