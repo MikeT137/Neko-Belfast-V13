3a1ad0758352de65ba448a84d2b223542091d954
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-02 12:31:10 +0300

    Update pat.js

--- commands/pat.js
+++ commands/pat.js
@@ -8,6 +8,11 @@ module.exports = {
             'https://media.giphy.com/media/4HP0ddZnNVvKU/giphy.gif',
             'https://media.giphy.com/media/109ltuoSQT212w/giphy.gif',
             'https://media.giphy.com/media/ye7OTQgwmVuVy/giphy.gif',
+            'https://media.tenor.com/images/a671268253717ff877474fd019ef73e9/tenor.gif',
+            'https://i.pinimg.com/originals/2e/62/cd/2e62cd7491be4ec9f0ec210d648b80fd.gif',
+            'https://thumbs.gfycat.com/FlimsyDeafeningGrassspider-size_restricted.gif',
+            'https://em.wattpad.com/7eb08d93b3a97f02bc611f78d8608d4fabe79951/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f4f4876343146766e4a7136665a773d3d2d3635313034303939312e31353634663135626330663632636161363839393833333730382e676966',
+            'https://thumbs.gfycat.com/AgileHeavyGecko-max-1mb.gif',
         ]
         if(message.mentions.members.size == 1) {
             let member = message.mentions.members.first()
