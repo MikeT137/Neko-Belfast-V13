20dbe76d212da07ad274428ec1207e1dccbeb31f
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-02 14:22:41 +0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,7 +2,7 @@ module.exports = {
     name:'avatar',
     description: "Shows you your avatar",
     execute(message, argument){
-        if (!argument[1]){
+        if (!argument[2]){
             const user = message.author;
             return message.channel.send(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
         }
