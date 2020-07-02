68dcda0807837ff808d825dca17569a567af4922
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-02 22:35:57 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,8 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
             .setTitle('Help')
             .addField('Prefix: ', PREFIX)
-            .addField('Commands', 'avatar - shows your avatar\nclear - clears the amount of messages you want\nhelp - shows this message\ninfo - shows bot information\nlove - a lovely message from the bot\nmute - mutes someone for a certain period of time')
+            .addField('Commands', 'clear - clears the amount of messages you want\nhelp - shows this message\ninfo - shows bot information\nmute - mutes someone for a certain period of time')
+            .addField('Self commands', 'avatar, cry, laugh, love, smile')
             .addField('Interactive commands (@someone)', 'handhold, hug, kiss, lick, pat, poke, punch, slap, snuggle')
             .setColor(0x4AEFBA)
             message.channel.send(embed);
