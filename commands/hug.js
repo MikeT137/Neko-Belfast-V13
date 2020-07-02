06b410709be38691cfa838aa2748d938e1ea0b57
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-02 12:31:18 +0300

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -11,6 +11,8 @@ module.exports = {
             'https://media.giphy.com/media/xJlOdEYy0r7ZS/giphy.gif',
             'https://media.giphy.com/media/DjczAlIcyK1Co/giphy.gif',
             'https://media.giphy.com/media/aD1fI3UUWC4/giphy.gif',
+            'https://acegif.com/wp-content/uploads/anime-hug.gif',
+            'https://media.tenor.com/images/2e1d34d002d73459b6119d57e6a795d6/tenor.gif',
         ]
         if(message.mentions.members.size == 1) {
             let member = message.mentions.members.first()
