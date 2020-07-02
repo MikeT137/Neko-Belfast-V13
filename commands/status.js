38ad1b663139cc9c70d057537a0416bee13afc47
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-02 13:14:34 +0300

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -4,5 +4,5 @@ module.exports = bot => {
     setInterval(function() {
         let status = "b!help";
         bot.user.setActivity(status, {type:"Playing"});
-    });
+    }, 9999999999999999999999999)
 }
