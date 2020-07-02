1407777584a141e1a7b96b8f23fdb4c1d0463635
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-02 13:10:42 +0300

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -3,6 +3,6 @@ const Discord = require('discord.js');
 module.exports = bot => {
     setInterval(function() {
         let status = "b!help";
-        bot.user.setActivity(status, {type:"WATCHING "});
+        bot.user.setActivity(status, {type:"Playing"});
     });
 }
