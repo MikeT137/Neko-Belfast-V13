80640e96556164ef341e89b69c75926b9b071eaf
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-02 14:38:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const PREFIX = 'b!';
+const PREFIX = 'bel ';
 
 const fs = require('fs');
 bot.commands = new Discord.Collection();
