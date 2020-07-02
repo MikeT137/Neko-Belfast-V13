2d3171b8b1d64999af3fad9629aeb02be8824f1c
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-02 12:31:15 +0300

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -9,6 +9,10 @@ module.exports = {
             'https://media.giphy.com/media/11rWoZNpAKw8w/giphy.gif',
             'https://media.giphy.com/media/jR22gdcPiOLaE/giphy.gif',
             'https://media.giphy.com/media/flmwfIpFVrSKI/giphy.gif',
+            'https://acegif.com/wp-content/uploads/anime-kiss-m.gif',
+            'https://thumbs.gfycat.com/WarpedSlightFrigatebird-size_restricted.gif',
+            'https://pa1.narvii.com/6836/cf4d3f8516103d493f7f7d6a18cc3082eca0a063_hq.gif',
+            'https://i.gifer.com/QPB7.gif',
         ]
         if(message.mentions.members.size == 1) {
             let member = message.mentions.members.first()
