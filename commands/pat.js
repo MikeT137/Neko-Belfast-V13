4c3a3e098f813dca015854fcf8c8f2f2c9fb1469
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-02 14:27:36 +0300

    Update pat.js

--- commands/pat.js
+++ commands/pat.js
@@ -17,7 +17,7 @@ module.exports = {
         ]
         if(message.mentions.members.size == 1) {
             let member = message.mentions.members.first()
-            message.channel.send(`${user.username} pats ${member.username}...so cute`);
+            message.channel.send(`${user.username} pats ${member}...so cute`);
             message.channel.send(random_pat[Math.floor(Math.random() * random_pat.length)]);
         }
     }
