70548697154aba57d5b8042191e5c0c72254e5f2
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-03 08:29:45 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ module.exports = {
             .setTitle('Help')
             .addField('Prefix: ', PREFIX)
             .addField('Commands', 'clear - clears the amount of messages you want\nhelp - shows this message\ninfo - shows bot information\nmute - mutes someone for a certain period of time')
-            .addField('Self commands', 'avatar, cry, laugh, love, smile')
+            .addField('Self commands', 'avatar, cry, laugh, love, smile, smug')
             .addField('Interactive commands (@someone)', 'handhold, hug, kiss, lick, pat, poke, punch, slap, snuggle')
             .setColor(0x4AEFBA)
             message.channel.send(embed);
