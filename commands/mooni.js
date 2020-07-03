51b0f8cd2ba77d51204b28c9271d6982dde4ecd6
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-03 08:41:40 +0300

    Update mooni.js

--- commands/mooni.js
+++ commands/mooni.js
@@ -7,7 +7,7 @@ module.exports = {
         if(!argument[1]) {
             const embed = new Discord.MessageEmbed()
 
-            .addField('I love you Mooni!', 'I wuv you so much my little moon, i want to stay with you forever and snuggle with you a olt and maybe even some other things >w<')
+            .addField('I love you Mooni!', 'I wuv you so so much my little moon, i want to stay with you forever and snuggle with you a lot and maybe even some other things >w<')
             .setImage('https://gifimage.net/wp-content/uploads/2017/11/i-love-you-anime-gif-11.gif')
             .setColor(0x4AEFBA)
             .setFooter('Sincerely, Miku')
