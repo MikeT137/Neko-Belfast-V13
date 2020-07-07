898dd8bf49f73b9d147bdad836e77d473b27c4f0
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-07 17:02:10 +0300

    Create kick.js

---
+++ commands/kick.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name:'kick',
+    description: "Kicks a user",
+    execute(message, argument){
+        if(!argument[1]) return message.reply('Please specify a member');
+
+        const person = message.mentions.users.first();
+        if(person) {
+            person.kick.then(() => {
+                message.channel.send(`Successfullky kicked ${person.tag}`);
+            }) = catch(err => {
+                message.channel.send('I was unnable to kick that user');
+            });
+        }else {
+            message.channel.send('That user isnt in the server')
+        }
+    }
+}
