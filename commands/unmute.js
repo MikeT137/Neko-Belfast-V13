2521a22214f7872ac430dc4856a25bad8cb265ec
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-07 23:17:17 +0300

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -8,14 +8,15 @@ module.exports = {
         let person = message.guild.member(message.mentions.users.first())
         
         if(!muterole) return message.channel.send('Couldnt find the mute role');
-
-        if (argument[1] == person){
-            person.roles.remove(muterole.id);
-            message.channel.send(`@${person.user.tag} has been unmuted`);
-        }else if (!argument[1]){
-            message.channel.send('Please specify a muted member')
-        }else{
-            message.channel.send('You typed the wrong arguments');
+        else {
+            if (argument[1] == person){
+                person.roles.remove(muterole.id);
+                message.channel.send(`@${person.user.tag} has been unmuted`);
+            }else if (!argument[1]){
+                message.channel.send('Please specify a muted member')
+            }else{
+                message.channel.send('You typed the wrong arguments');
+            }
         }
     }
 }
