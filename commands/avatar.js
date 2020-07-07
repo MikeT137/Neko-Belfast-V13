334ae8bda83e9103c9804deac9c03a2bf61fd49f
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-07 22:16:45 +0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -5,12 +5,12 @@ module.exports = {
         const author = message.author;
         const person = message.mentions.users.first();
 
-        if (!argument[1]){
+        if (!argument[1])
             return message.channel.send(`${author.username}'s avatar: ${author.displayAvatarURL({ dynamic: true })}`);
-        }else if (!argument[2] && message.mentions.members.size == 1) {
-            const embed = new Discord.MessageEmbed()
-            
+        else if (!argument[2] && message.mentions.members.size == 1)
             message.channel.send(`${person.username}s avatar: ${person.displayAvatarURL({ dynamic: true })}`);
-        }
+        else
+            message.channel.send('You typed therong arguments');
+        break;
     }
 }
