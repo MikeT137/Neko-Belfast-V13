445bd16a47e992e91f79e5def96a97386eb63595
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-07 22:40:33 +0300

    Create unmute.js

---
+++ commands/unmute.js
@@ -0,0 +1,21 @@
+module.exports = {
+    name:'unmute',
+    description: "Unmutes a user",
+    execute(message, argument){
+        const ms = require('ms');
+        let mainrole = message.guild.roles.cache.find(role => role.name === 'Member')
+        let muterole = message.guild.roles.cache.find(role => role.name === 'BMuted')
+        let person = message.guild.member(message.mentions.users.first() || message.guild.members.get(argument[1]))
+        
+        if(!muterole) return message.channel.send('Couldnt find the mute role');
+
+        if(!person) {
+            message.channel.send('Please specify a member');
+        }else if(person){
+            person.roles.remove(muterole.id);
+            message.channel.send(`@${person.user.tag} has been unmuted`);
+        }else {
+            message.channel.send('You typed the wrong arguments');
+        }
+    }
+}
