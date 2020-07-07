d9c483001c10d07267338c4c37b2e2b0ab57d0b9
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-07 17:19:15 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -5,14 +5,19 @@ module.exports = {
         if(!argument[1]) return message.channel.send('Please specify a member');
 
         const person = message.mentions.users.first();
+
         if(person) {
-            person.kick().then(() => {
-                message.channel.send(`Successfullky kicked ${person.tag}`);
-            }).catch(err => {
-                message.channel.send('I was unnable to kick that user');
-            });
-        }else {
-            message.channel.send('That user isnt in the server')
+            const user = member.guild.member(user);
+
+            if(user) {
+                user.kick().then(() => {
+                    message.channel.send(`Successfullky kicked ${user.tag}`);
+                }).catch(err => {
+                    message.channel.send('I was unnable to kick that member');
+                });
+            }else {
+                message.channel.send('That member isnt in the server')
+            }
         }
     }
 }
