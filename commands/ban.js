c97cc15dfe8ed0258e660ffdbf6a8d3f314a9f65
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-07 18:10:06 +0300

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name:'ban',
+    description: "Bans a user",
+    execute(message, argument){
+        const member = message.mentions.members.first();
+
+        if(!argument[1]) return message.channel.send('Please specify a member');
+
+        const person = message.mentions.users.first();
+
+        if(person) {
+            const user = member.guild.member(person);
+
+            if(user) {
+                user.ban().then(() => {
+                    message.channel.send(`Successfullky banned ${person.tag}`);
+                }).catch(err => {
+                    message.channel.send('I was unnable to ban that member');
+                });
+            }else {
+                message.channel.send('That member isnt in the server')
+            }
+        }
+    }
+}
