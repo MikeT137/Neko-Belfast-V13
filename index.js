f05ecfde1621081ff8b972206b849b0a54d6d7b2
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-07 17:50:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ bot.on('ready', () => {
 });
 
 bot.on('message', (message) => {
-    if(message.content == 'hey' || message.content == 'hi')
+    if(message.content == 'hey' || message.content == 'hi' || message.content == 'Hey' || message.content == 'Hi')
          message.channel.send('hewwo');
 });
 
