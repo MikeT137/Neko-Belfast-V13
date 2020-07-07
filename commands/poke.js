f7c84210461f8769c3e52fe1fcf488c7d9e16f37
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-07 18:01:50 +0300

    Update poke.js

--- commands/poke.js
+++ commands/poke.js
@@ -3,6 +3,8 @@ module.exports = {
     description: "Pokes a user",
     execute(message, argument){
         const Discord = require('discord.js');
+        const author = message.author;
+        const person = message.mentions.users.first();
         const random_poke = [
             'https://media.giphy.com/media/pWd3gD577gOqs/giphy.gif',
             'https://media.giphy.com/media/aZSMD7CpgU4Za/giphy.gif',
@@ -18,6 +20,7 @@ module.exports = {
         if(!argument[2] && message.mentions.members.size == 1) {
             const embed = new Discord.MessageEmbed()
 
+            .setTitle(`${author.username} gave ${person.username} a poke`)
             .setImage(random_poke[Math.floor(Math.random() * random_poke.length)])
             .setColor(0x4AEFBA)
             message.channel.send(embed);
