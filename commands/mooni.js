462d5ca075f3c7c6e0fc03c016d084504af8faf1
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-07 18:53:38 +0300

    Update mooni.js

--- commands/mooni.js
+++ commands/mooni.js
@@ -7,7 +7,7 @@ module.exports = {
         if(!argument[1]) {
             const embed = new Discord.MessageEmbed()
 
-            .addTitle('Thank ouy for accepting such a kid like me in your "life" and for everything you done so far. I always look up to you because of how cool and amazing person you are, and believe me, you changed my life for the better and ill be loyal to you no matter what, And for that...i cant be grateful enough, i wuv you')
+            .setTitle('Thank ouy for accepting such a kid like me in your "life" and for everything you done so far. I always look up to you because of how cool and amazing person you are, and believe me, you changed my life for the better and ill be loyal to you no matter what, And for that...i cant be grateful enough, i wuv you')
             .setImage('https://media.giphy.com/media/hnNyVPIXgLdle/giphy.gif')
             .setColor(0x4AEFBA)
             .setFooter('Sincerely, Miku')
