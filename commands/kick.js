4c2f6aa2a1063763ee89ccb0a1df83aca8a4bf20
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-07 17:09:33 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,7 +2,7 @@ module.exports = {
     name:'kick',
     description: "Kicks a user",
     execute(message, argument){
-        if(!argument[1]) return message.reply('Please specify a member');
+        if(!argument[1]) return message.channel.send('Please specify a member');
 
         const person = message.mentions.users.first();
         if(person) {
