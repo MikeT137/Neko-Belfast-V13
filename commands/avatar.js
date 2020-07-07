156ec523fab58fa178632b6a6058ba86e0cc5f5a
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-07 22:25:08 +0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,15 +2,23 @@ module.exports = {
     name:'avatar',
     description: "Shows you your avatar",
     execute(message, argument){
+        const Discord = require('discord.js');
+        const embed = new Discord.MessageEmbed()
         const author = message.author;
         const person = message.mentions.users.first();
 
-        if (!argument[1])
-            message.channel.send(`${author.username}'s avatar: ${author.displayAvatarURL({ dynamic: true })}`);
-        else if (!argument[2] && message.mentions.members.size == 1)
-            message.channel.send(`${person.username}s avatar: ${person.displayAvatarURL({ dynamic: true })}`);
-        else {
-            message.channel.send('You typed therong arguments');
+        if (!argument[1]){
+            .setTitle(`${author.username}'s avatar:`);
+            .setImage(`${author.displayAvatarURL({ dynamic: true })}`)
+            .setColor(0x4AEFBA)
+            message.channel.send(embed);
+        }else if (!argument[2] && message.mentions.members.size == 1) {
+            .setTitle(`${person.username}s avatar:`);
+            .setImage(`${person.displayAvatarURL({ dynamic: true })}`)
+            .setColor(0x4AEFBA)
+            message.channel.send(embed);
+        }else {
+            message.channel.send('You typed the wrong arguments');
         }
     }
 }
