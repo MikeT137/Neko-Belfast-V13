9e39d70908dd64970a8883d64428b7c8436463e1
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-10 17:49:53 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ module.exports = {
             .setTitle('Help')
             .addField('Prefix: ', PREFIX)
             .addField('Modertion Commands', 'ban - bans a member\nclear - clears the amount of messages you want\nhelp - shows this message\ninfo - shows bot information\nkick - kicks a member\nmute/unmute - mutes/unmutes someone')
-            .addField('Self commands', 'avatar, cry, dance, laugh, love, smile, smug')
+            .addField('Self commands', 'avatar, clap, cry, dance, laugh, love, smile, smug') //mooni
             .addField('Interactive commands (@someone)', 'handhold, hug, kiss, lick, pat, poke, punch, slap, snuggle')
             .setColor(0x4AEFBA)
             message.channel.send(embed);
