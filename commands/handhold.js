8129687b466b0dc225d081b51204aa899cd6f54f
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-10 12:43:52 +0300

    Update handhold.js

--- commands/handhold.js
+++ commands/handhold.js
@@ -17,13 +17,15 @@ module.exports = {
             'https://i.kym-cdn.com/photos/images/original/001/100/981/002.gif',
             'https://data.whicdn.com/images/248505351/original.gif',
         ]
-        if(!argument[2] && message.mentions.members.size >= 1) {
+        if(message.mentions.members.size >= 1) {
             const embed = new Discord.MessageEmbed()
 
             .setTitle(`${author.username} holds ${person.username}s hand...:flushed:`)
             .setImage(random_hh[Math.floor(Math.random() * random_hh.length)])
             .setColor(0x4AEFBA)
             message.channel.send(embed);
+        }else if(argument[1] == `${author.username}`) {
+            message.channel.send('Aww, ill hold your hand :blush:')
         }
     }
 }
