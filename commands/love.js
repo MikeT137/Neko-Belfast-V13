dacc76b882cefb7ae5630caad280b55eaba9736d
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-11 10:48:09 +0300

    Delete love.js

--- commands/love.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name:'love',
-    description: "The bot give you a lovely message",
-    execute(message, argument){
-        if(!argument[1]){
-            message.channel.send('I love you master!:heart:');
-        }
-    }
-}
