764b199ed434b76cae07e20b868899942454a0d2
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-17 10:33:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ bot.on('message', message => {
             break;
 
             case 'warn':
-                bot.commands.get('warn').execute(message, argument);
+                bot.commands.get('warn').run(message, argument);
             break;
         }
     }
