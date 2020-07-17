437eec187dc8b944eb727fe0db9e0e47e66e50db
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-17 11:57:33 +0300

    Update poke.js

--- commands/poke.js
+++ commands/poke.js
@@ -17,7 +17,7 @@ module.exports = {
             'https://pa1.narvii.com/5679/d39dc10bcad2fd42a130de685d192c166d55f69a_hq.gif',
             'https://steamuserimages-a.akamaihd.net/ugc/957465844206544934/F541AE935C936C3BAF5250F8D2606CFECE17A2DA/',
         ]
-        if(!argument[2] && message.mentions.members.size == 1) {
+        if(message.mentions.members.size >= 1) {
             const embed = new Discord.MessageEmbed()
 
             .setTitle(`${author.username} gave ${person.username} a poke`)
