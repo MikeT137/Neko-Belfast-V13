40aa5c3fa6b53721a41122d265f41d838542f1f4
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-18 20:39:38 +0300

    Update love.js

--- commands/love.js
+++ commands/love.js
@@ -1,7 +1,7 @@
 module.exports = {
     name:'love',
     description: "The bot gives a nice message and snuggles the user",
-    run: async (bot, message, argument){
+    execute(message, argument){
         const author = message.author;
         const Discord = require('discord.js');
         const random_snuggle = [
