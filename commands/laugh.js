8ce92ea2d04f1e4342ca362e9f89590585e9bfa1
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-18 20:27:25 +0300

    Update laugh.js

--- commands/laugh.js
+++ commands/laugh.js
@@ -1,7 +1,7 @@
 module.exports = {
     name:'laugh',
     description: "The user laughs",
-    execute(message, argument){
+    run: async (bot, message, argument){
         const author = message.author;
         const Discord = require('discord.js');
         const random_laugh = [
