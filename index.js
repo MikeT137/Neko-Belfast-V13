61d5b329fe3e38468ce2fa67a0ac960dda243e35
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-18 16:16:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const bot = new Discord.Client();
 
 const PREFIX = 'b!';
 
-const cheerio = require('cheerio');
-const request = require('request');
-
 const fs = require('fs');
 bot.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
@@ -177,34 +174,11 @@ bot.on('message', message => {
                 bot.commands.get('facepalm').execute(message, argument);
             break;
 
-            case 'image':
-                image(message);
+            case 'meme':
+                bot.commands.get('meme').execute(message, argument);
             break;
         }
     }
 });
-function image(message){
-    var options = {
-        url: "http://results.dogpile.com/serp?qc=images&q=" + "memes",
-        method: "GET",
-        headers: {
-            "Accept": "text/html",
-            "User-Agent": "Chrome"
-        }
-    }
-    request(options, function(error, response, responseBody) {
-        if (error) {
-            return;
-        }
-        $ = cheerio.load(responseBody);
-        var links = $(".image a.link");
-        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
-        console.log(urls);
 
-        if (!urls.length) {
-            return;
-        }
-        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
-    });
-}
 bot.login(process.env.token);
