b32b42d0f874bb5084aa477aa2c13778381cbb36
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-18 20:39:35 +0300

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,7 +1,7 @@
 module.exports = {
     name:'mute',
     description: "Mutes a user temporarily",
-    run: async (bot, message, argument){
+    execute(message, argument){
         const ms = require('ms');
         let person = message.guild.member(message.mentions.users.first() || message.guild.members.get(argument[2]))
         
