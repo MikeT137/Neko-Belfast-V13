e06178ac992b5ab1e6b3fdbad41f0f279b800b22
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-18 20:39:24 +0300

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -1,7 +1,7 @@
 module.exports = {
     name:'slap',
     description: "Slaps a user",
-    run: async (bot, message, argument){
+    execute(message, argument){
         const Discord = require('discord.js');
         const author = message.author;
         const person = message.mentions.users.first();
