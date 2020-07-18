81861ded2a0a8cf176f5d06570942c960cf01935
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-18 20:27:04 +0300

    Update dance.js

--- commands/dance.js
+++ commands/dance.js
@@ -1,7 +1,7 @@
 module.exports = {
     name:'dance',
     description: "The user dances",
-    execute(message, argument){
+    run: async (bot, message, argument){
         const author = message.author;
         const Discord = require('discord.js');
         const random_dance = [
