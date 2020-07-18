5d56717ed86344acb98104c6cb95f4a0e37c3523
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-18 20:26:59 +0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,7 +1,7 @@
 module.exports = {
     name:'clear',
     description: "Clears the ammount of message you want",
-    execute(message, argument){
+    run: async (bot, message, argument){
         if(!argument[1]) return message.reply('Error, please specify how many messages i should clear');
         message.channel.bulkDelete(argument[1]);
     }
