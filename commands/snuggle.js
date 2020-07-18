ed7cfede25648de5ed38595aead017cc6c3624b9
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-18 20:39:19 +0300

    Update snuggle.js

--- commands/snuggle.js
+++ commands/snuggle.js
@@ -1,7 +1,7 @@
 module.exports = {
     name:'snuggle',
     description: "Snuggles a user",
-    run: async (bot, message, argument){
+    execute(message, argument){
         const Discord = require('discord.js');
         const author = message.author;
         const person = message.mentions.users.first();
