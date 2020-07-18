71efc2f97ef214124292d703bf52d41988b6007d
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-18 20:39:56 +0300

    Update handhold.js

--- commands/handhold.js
+++ commands/handhold.js
@@ -1,7 +1,7 @@
 module.exports = {
     name:'handhold',
     description: "Hold a user's hand",
-    run: async (bot, message, argument){
+    execute(message, argument){
         const Discord = require('discord.js');
         const author = message.author;
         const person = message.mentions.users.first();
