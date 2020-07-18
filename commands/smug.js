a44c67b6403a2c4718cb02ef328f7003aa17c91a
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-18 20:39:21 +0300

    Update smug.js

--- commands/smug.js
+++ commands/smug.js
@@ -1,7 +1,7 @@
 module.exports = {
     name:'smug',
     description: "The user smugs",
-    run: async (bot, message, argument){
+    execute(message, argument){
         const author = message.author;
         const Discord = require('discord.js');
         const random_smug = [
