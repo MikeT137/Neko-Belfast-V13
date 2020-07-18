1e88ff9886c122e0734316d6527277b1efe5885d
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-18 20:40:10 +0300

    Update blush.js

--- commands/blush.js
+++ commands/blush.js
@@ -1,7 +1,7 @@
 module.exports = {
     name:'blush',
     description: "The user blushes",
-    run: async (bot, message, argument){
+    execute(message, argument){
         const author = message.author;
         const Discord = require('discord.js');
         const random_blush = [
