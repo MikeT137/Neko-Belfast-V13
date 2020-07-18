7d55a542ee7a6576942ffcada9a7fd629500f8ea
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-18 20:27:28 +0300

    Update lewd.js

--- commands/lewd.js
+++ commands/lewd.js
@@ -1,7 +1,7 @@
 module.exports = {
     name:'lewd',
     description: "The user thinks whatever happend is lewd",
-    execute(message, argument){
+    run: async (bot, message, argument){
         const author = message.author;
         const Discord = require('discord.js');
         const random_lewd = [
