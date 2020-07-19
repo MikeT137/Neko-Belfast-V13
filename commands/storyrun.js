7ff6342f7f22f4ea78b646112803a158b1456b5d
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-19 12:52:16 +0300

    Update storyrun.js

--- commands/storyrun.js
+++ commands/storyrun.js
@@ -10,11 +10,7 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
 
             .setTitle('Story - RUN')
-            .addField('Youre a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:')
-            .addField('\n(b!1)still chop wood')
-            .addField('  2)go inside your house')
-            .addField('  3)take your axe with you and investigate the zone')
-            .addField('\nSelect your choice:')
+            .addField('Youre a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:', '\n(b!1)still chop wood\n  2)go inside your house\n  3)take your axe with you and investigate the zone\n\nSelect your choice:')
             .setColor(0x4AEFBA)
             message.author.send(embed);
         }
