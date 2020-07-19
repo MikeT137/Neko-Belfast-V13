ac7861c0a5ee1fae3e20e981c5cbe60f466a8c9d
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-19 17:22:05 +0300

    Update storyrun.js

--- commands/storyrun.js
+++ commands/storyrun.js
@@ -7,16 +7,8 @@ module.exports = {
         if(!argument[1]) {
             message.channel.send(`${message.author.username}, look in your DM for the story! (dont give spoilers)`)
 
-            const embed = new Discord.MessageEmbed()
-
-            .setTitle('Story - RUN')
-            .setTitle('Youre a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:')
-            .setTitle('\n(b!1)still chop wood')
-            .setTitle('  2)go inside your house')
-            .setTitle('  3)take your axe with you and investigate the zone')
-            .setTitle('\nSelect your choice:')
-            .setColor(0x4AEFBA)
-            message.author.send(embed);
+            message.author.send('Story - RUN\nYoure a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:\n(b!1)still chop wood\n  2)go inside your house\n  3)take your axe with you and investigate the zone\n\nSelect your choice:');
+            
         }
     }
 }
