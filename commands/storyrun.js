1eb09986c3c5b6ffeb6b1e6d412d3056ccb85642
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-19 12:39:38 +0300

    Create storyrun.js

---
+++ commands/storyrun.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name:'storyrun',
+    description: "The user playes the story called run",
+    execute(message, argument){
+        const Discord = require('discord.js');
+
+        if(!argument[2]) {
+            message.channel.send(`${message.author.username}, look in your DM for the story! (dont give spoilers)`)
+
+            const embed = new Discord.MessageEmbed()
+            
+            .setTitle('Story - RUN')
+            .addField('Youre a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:')
+            .addField('\n(b!1)still chop wood')
+            .addField('  2)go inside your house')
+            .addField('  3)take your axe with you and investigate the zone')
+            .addField('\nSelect your choice:')
+            .setColor(0x4AEFBA)
+            message.author.send(embed);
+        }
+    }
+}
