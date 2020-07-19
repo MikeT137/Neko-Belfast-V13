7d08a00cda58b742d904c6c5b7312d7c0eaf9f1a
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-19 17:29:22 +0300

    Update storyrun.js

--- commands/storyrun.js
+++ commands/storyrun.js
@@ -6,7 +6,7 @@ module.exports = {
 
         if(!argument[1]) {
             message.channel.send(`${message.author.username}, look in your DM for the story! (dont give spoilers)`)
-            message.author.send('   Story - RUN\nYoure a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:\n(b!1)still chop wood\n   2)go inside your house\n   3)take your axe with you and investigate the zone\n\nSelect your choice:');
+            message.author.send('   Story - RUN\nYoure a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:\nb!1)still chop wood\n   2)go inside your house\n   3)take your axe with you and investigate the zone\nSelect your choice:');
             
         }
     }
