6c9fff631a268cf14ae15dbf626c8137ae468630
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-20 11:21:50 +0300

    Update storyrun.js

--- commands/storyrun.js
+++ commands/storyrun.js
@@ -5,10 +5,10 @@ module.exports = {
         const Discord = require('discord.js');
 
         if(!argument[1]) {
-            let msg = await message.channel.send('   Story - RUN\nYoure a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:\nb!1)still chop wood\n   2)go inside your house\n   3)take your axe with you and investigate the zone\nSelect your choice:');
-            await msg.channel.react('👍');
-            await msg.channel.react('✋');
-            await msg.channel.react('👎');
+            let msg = message.channel.send('   Story - RUN\nYoure a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:\nb!1)still chop wood\n   2)go inside your house\n   3)take your axe with you and investigate the zone\nSelect your choice:');
+            msg.channel.react('👍');
+            msg.channel.react('✋');
+            msg.channel.react('👎');
 
             message.awaitReactions(filter, {max: 1})
             .then(collected => {
@@ -22,7 +22,7 @@ module.exports = {
                     message.channel.send('bish');
                 }
             });
-            
+
             if(message.content === 'b!1') {
                 message.author.send('You think to yourself that it was just an animal or something, so you decide to ignore it. You then finish chopping the wood, you grab all of it, and go towards your basement so you can leave it there. But then you hear that weird sound again, this time coming from your basement. You then:\nb!1)go in the basement\n   2)leave the cabin\nSelect your choice:');
 
