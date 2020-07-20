c24d52dd3f63f1b4fba4e5f1834456d961f7edfe
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-20 10:46:18 +0300

    Update storyrun.js

--- commands/storyrun.js
+++ commands/storyrun.js
@@ -5,8 +5,8 @@ module.exports = {
         const Discord = require('discord.js');
 
         if(!argument[1]) {
-            message.channel.send('   Story - RUN\nYoure a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:\nb!1)still chop wood\n   2)go inside your house\n   3)take your axe with you and investigate the zone\nSelect your choice:');
-            message.react('👍')
+            message.channel.send('   Story - RUN\nYoure a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:\nb!1)still chop wood\n   2)go inside your house\n   3)take your axe with you and investigate the zone\nSelect your choice:')
+            .react('👍')
             .then(() => message.react('✋'))
             .then(() => message.react('👎'))
             .catch(() => console.error('One of the emojis failed to react.'));
