7aa1edec644bb1bb47d6b042fbe1cf7e29b67bef
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-20 10:24:02 +0300

    Update storyrun.js

--- commands/storyrun.js
+++ commands/storyrun.js
@@ -5,34 +5,8 @@ module.exports = {
         const Discord = require('discord.js');
 
         if(!argument[1]) {
-            message.channel.send(`${message.author.username}, look in your DM for the story! (dont give spoilers)`)
-            message.author.send('   Story - RUN\nYoure a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:\nb!1)still chop wood\n   2)go inside your house\n   3)take your axe with you and investigate the zone\nSelect your choice:');
-            message.react(':one:')
-            .then(() => message.react(':two:'))
-            .then(() => message.react(':three:'))
-            .catch(() => console.error('One of the emojis failed to react.'));
-
-            const filter = (reaction, user) => {
-                return [':one:', ':two:', ':three:'].includes(reaction.emoji.name) && user.id === message.author.id;
-            };
+            message.channel.send('   Story - RUN\nYoure a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:\nb!1)still chop wood\n   2)go inside your house\n   3)take your axe with you and investigate the zone\nSelect your choice:');
             
-            message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-                .then(collected => {
-                    const reaction = collected.first();
-            
-                    if (reaction.emoji.name === ':one:') {
-                        message.author.send('you reacted with a thumbs up.');
-                    }else if(reaction.emoji.name === ':two:') {
-                        message.author.send('you reacted with a thumbs down.');
-                    }else {
-                        message.author.send('bish');
-                    }
-                })
-                .catch(collected => {
-                    console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
-                    message.reply('you didn\'t react with neither a thumbs up, nor a thumbs down.');
-                });
-
             if(message.content === 'b!1') {
                 message.author.send('You think to yourself that it was just an animal or something, so you decide to ignore it. You then finish chopping the wood, you grab all of it, and go towards your basement so you can leave it there. But then you hear that weird sound again, this time coming from your basement. You then:\nb!1)go in the basement\n   2)leave the cabin\nSelect your choice:');
 
