96e7c7a179c5455acd202b7c9eb8309feefa82f0
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-21 10:14:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -179,7 +179,26 @@ bot.on('message', message => {
             break;
 
             case 'meme':
-                bot.commands.get('meme').run(message, argument);
+                const randomPuppy = require('random-puppy');
+                const subreddits = [
+                    "dankmemes",
+                    "memes",
+                    "Animemes",
+                    "wholesomememes",
+                    "wholesomeanimemes",
+                ]
+                const random = subreddits[Math.floor(Math.random() * subreddits.length)];
+                const img = randomPuppy(random);
+        
+                if(!argument[1]) {
+                    const embed = new Discord.MessageEmbed()
+        
+                    .setTitle(`From r/${random}`)
+                    .setImage(img)
+                    .setURL(`htps://reddit.com/r/${random}`)
+                    .setColor(0x4AEFBA)
+                    message.channel.send(embed);
+                }
             break;
 
             /*case 'storyrun':
