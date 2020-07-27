1342144cd40534b0df24e4fc8a4594524f8b4e98
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-27 19:16:38 +0300

    Create mute.js

---
+++ commands/moderation/mute.js
@@ -0,0 +1,30 @@
+module.exports = {
+    name:'mute',
+    description: "Mutes a user temporarily",
+    run: async (bot, message, args) => {
+        const ms = require('ms');
+        const person = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+        
+        if(!message.member.hasPermission("MANAGE_MESSAGES") || !message.guild.owner) return message.channel.send('You dont have permissions to use this command');
+        if(!person) return message.channel.send('Please specify a member');
+        
+        let muterole = message.guild.roles.cache.find(role => role.name === 'BMuted')
+
+        if(!muterole) return message.channel.send('Couldnt find the mute role');
+
+        const time = args[1];
+
+        if(!time){
+            return message.channel.send('You didnt specify the time');
+        }else{
+        person.roles.add(muterole.id);
+        message.channel.send(`@${person.user.tag} has been muted for ${ms(ms(time))}`)
+
+        setTimeout(function(){
+            person.roles.remove(muterole.id);
+            message.channel.send(`@${person.user.tag} has been unmuted`);
+
+        }, ms(time));
+        }
+    }
+}
