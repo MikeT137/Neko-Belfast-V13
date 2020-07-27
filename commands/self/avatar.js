d3771c7430496057278bc0ac07feaf1d069df5fa
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-27 19:16:17 +0300

    Create avatar.js

---
+++ commands/self/avatar.js
@@ -0,0 +1,27 @@
+module.exports = {
+    name:'avatar',
+    description: "Shows you your avatar",
+    run: async (bot, message, args) => {
+        const Discord = require('discord.js');
+        const author = message.author;
+        const person = message.mentions.users.first();
+
+        if (!args[0]){
+            const embed = new Discord.MessageEmbed()
+
+            .setTitle(`${author.username}'s avatar:`)
+            .setImage(`${author.displayAvatarURL({ dynamic: true })}`)
+            .setColor(0x4AEFBA)
+            message.channel.send(embed);
+        }else if (!args[1] && message.mentions.members.size == 1) {
+            const embed = new Discord.MessageEmbed()
+
+            .setTitle(`${person.username}'s avatar:`)
+            .setImage(`${person.displayAvatarURL({ dynamic: true })}`)
+            .setColor(0x4AEFBA)
+            message.channel.send(embed);
+        }else {
+            message.channel.send('You typed the wrong arguments');
+        }
+    }
+}
