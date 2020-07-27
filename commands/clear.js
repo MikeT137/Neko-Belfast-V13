b7450f5ba9cc97582ab6dd090f57c0d314c857b6
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-07-27 19:15:05 +0300

    Delete clear.js

--- commands/clear.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name:'clear',
-    description: "Clears the ammount of message you want",
-    execute(message, argument){
-        if(!argument[1]) return message.reply('Error, please specify how many messages i should clear');
-        message.channel.bulkDelete(argument[1]);
-    }
-}
