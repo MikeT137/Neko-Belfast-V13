b8050b21ab4ce52e4eb096b36b52af35108c0fc2
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-08-04 12:34:13 +0300

    dfg

--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -1,14 +1,13 @@
-const message = require("./message")
-
 module.exports = async(message) => {
     const embed = new Discord.MessageEmbed()
 
     .setTitle('New message deleted!')
     .setDescription(`**The user ${message.author.tag}* has deleted a message in <#${message.channel.id}>*`)
     .addField('Content', message.content, true)
+    .setColor(0x4AEFBA)
 
-    let channel = message.guild.channels.cache.find(ch => ch.name==='bot-log' || 'logs' || 'staff-logs'|| '📈┊staff-logs')
+    let channel = message.guild.channels.cache.find(ch => ch.name==='bot-log')
     if(!channel) return;
-    
+
     channel.send(embed);
 }
