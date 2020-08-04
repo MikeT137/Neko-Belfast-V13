e53bcd0008e0e46538844ce878e7cc52f694f05d
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-08-04 21:18:17 +0300

    Update math.js

--- commands/fun/math.js
+++ commands/fun/math.js
@@ -10,14 +10,14 @@ module.exports = {
         let resp;
 
         try{
-            resp = math.eval(args.join(' '));
+            resp = math.evaluate(args.join(' '));
         }catch(e){
             message.channel.send('Sorry, please input a valid calculation')
         }
         const embed = new Discord.MessageEmbed()
 
         .setTitle('Calculation')
-        .addField('Question', `\`\`\`css\n${args.join(' ')}\`\`\``)
+        .addField('Question', `\`\`\`css\n${args.join(' ')}\`\`\` = ?`)
         .addField('Answer', `\`\`\`css\n${resp}\`\`\``)
         .setColor(0x4AEFBA)
         message.channel.send(embed);
