bc01030d21a387351aedc1d1acb92f3093a231cb
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-08-04 14:26:42 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 module.exports = {
     calculator: function(num1, type, num2){
         if(type == '*') return num1 * num2;
-        if(type == '+') return num1 + num2;
+        if(type == '+') return (num1+num2);
         if(type == '-') return num1 - num2;
         if(type == '/') return num1 / num2;
         if(type == '^') return num1 ^ num2;
