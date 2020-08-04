ab359d26176aec961a4302145e562901b1c60cf3
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-08-04 10:42:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,54 @@ const prefix = config.prefix;
 const token = config.token;
 const fs = require('fs');
 
-//Command & Event Handler
+//Command Handler
 bot.commands = new Collection();
 bot.aliases = new Collection();
 bot.categories = fs.readdirSync("./commands/");
-["command", "event"].forEach(handler => {
+["command"].forEach(handler => {
     require(`./handlers/${handler}`)(bot);
 });
+
+bot.on('ready', () => {
+    require('./events/client/ready')(bot);
+});
+
+//Welcome Command
+bot.on('guildMemberAdd', member => {
+    require('./events/client/welcome')(member);
+})
+
+//Leaving Command
+bot.on('guildMemberRemove', member => {
+    require('./events/client/leave')(member);
+})
+
+//Hello command
+bot.on('message', (message) => {
+    require('./events/guild/hey')(bot, message);
+});
+
+//UwU command
+bot.on('message', (message) => {
+    if(message.content == 'uwu' || message.content == 'UwU' || message.content == 'UWU' || message.content == 'owo' || message.content == 'OwO' || message.content == 'OWO')
+        message.channel.send('>w<');
+});
+
+//Commands
+bot.on('message', async message => {
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+    if(!message.guild) return;
+    if(!message.member) message.member = await message.guild.forEach(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    const command = bot.commands.get(cmd);
+
+    if(cmd.length == 0) return;
+    if(!command) command = bot.commands.get(bot.aliases.get(cmd));
+    if(command) command.run(bot, message, args);
+})
 
 //Token
 bot.login(process.env.token);
