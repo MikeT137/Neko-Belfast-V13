a37abfd17c5a09f1ff2f4fe466b98219c50cc1b2
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-08-04 13:54:17 +0300

    df

--- index.js
+++ index.js
@@ -41,7 +41,18 @@ bot.on('leave', member => {
 
 //Commands
 bot.on('message', async (bot, message) => {
-    require('./events/guild/message')(bot, message);
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+    if(!message.guild) return;
+    if(!message.member) message.member = await message.guild.forEach(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    const command = bot.commands.get(cmd);
+
+    if(cmd.length == 0) return;
+    if(!command) command = bot.commands.get(bot.aliases.get(cmd));
+    if(command) command.run(bot, message, args);
 });
 
 //Token
