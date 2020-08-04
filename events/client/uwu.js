7e65661a3ae1612270786745ffb75707a9fe8732
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-08-04 09:31:51 +0300

    owo

---
+++ events/client/uwu.js
@@ -0,0 +1,4 @@
+module.exports = (message) => {
+    if(message.content == 'uwu' || message.content == 'UwU' || message.content == 'UWU' || message.content == 'owo' || message.content == 'OwO' || message.content == 'OWO')
+        message.channel.send('>w<');
+}
--- index.js
+++ index.js
@@ -15,26 +15,6 @@ bot.categories = fs.readdirSync("./commands/");
     require(`./handlers/${handler}`)(bot);
 });
 
-//Welcome Command
-bot.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.cache.find(channel => channel.name === 'welcome' || channel.name === 'Welcome' || channel.name === 'welcome-goodbye' || channel.name === 'Welcome-Goodbye' || channel.name === '👋┊welcome-goodbye' || channel.name === '👋┊Welcome-Goodbye')
-
-    channel.send(`Welcome ${member} to the server! Before doing anything, please read the rules of the server and...enjoy your stay ;3`)
-})
-
-//Leaving Command
-bot.on('guildMemberRemove', member => {
-    const channel = member.guild.channels.cache.find(channel => channel.name === 'welcome' || channel.name === 'Welcome' || channel.name === 'welcome-goodbye' || channel.name === 'Welcome-Goodbye' || channel.name === '👋┊welcome-goodbye' || channel.name === '👋┊Welcome-Goodbye')
-
-    channel.send(`${member} left the server, oof`)
-})
-
-//UwU command
-bot.on('message', (message) => {
-    if(message.content == 'uwu' || message.content == 'UwU' || message.content == 'UWU' || message.content == 'owo' || message.content == 'OwO' || message.content == 'OWO')
-        message.channel.send('>w<');
-});
-
 //Token
 bot.login(process.env.token);
 //bot.login(token);
