aaa2e2c8b3f0620542beea4a77ed880aa9e58f6e
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-08-05 10:10:33 +0300

    Update storyrun.js

--- commands/fun/storyrun.js
+++ commands/fun/storyrun.js
@@ -2,11 +2,14 @@ module.exports = {
     name:'storyrun',
     discription:'The user gets to play the creators story',
     run: async (bot, message, args) => {
-        message.channel.send('Youre a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:\n👎 - still chop wood\n✊ - go inside your house\n👍 - take your axe with you and investigate the zone\n**Select your choice:**')
-        await message.react('👎');
-        message.react('✊');
-        message.react('👍');
+        const Discord = require('dicsord.js');
         
+        const embed = new Discord.MessageEmbed()
+        .setTitle('Youre a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:\n**Select your choice:**')
+        .addField('\n👎 - still chop wood\n✊ - go inside your house\n👍 - take your axe with you and investigate the zone', '\n**Select your choice:**')
+        .setColor(0x4AEFBA)
+        message.channel.send(embed);
+
         if(message.react == '👎') {
             message.channel.send('skrrr')
         }else if(message.react == '✊') {
