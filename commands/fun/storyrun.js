dd80becdb977b8c5ec4f2f63c0dee8378545411b
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-08-05 10:13:01 +0300

    Update storyrun.js

--- commands/fun/storyrun.js
+++ commands/fun/storyrun.js
@@ -2,7 +2,7 @@ module.exports = {
     name:'storyrun',
     discription:'The user gets to play the creators story',
     run: async (bot, message, args) => {
-        const Discord = require('dicsord.js');
+        const Discord = require('discord.js');
         
         const embed = new Discord.MessageEmbed()
         .setTitle('Youre a 20 year old boy. You live in a small cabin inside a forest. Youre there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:\n**Select your choice:**')
