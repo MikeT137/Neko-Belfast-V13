90876e4b8f65364025fde803a027bd9c2a64beae
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-08-10 19:21:47 +0300

    Update bj.js

--- commands/fun/bj.js
+++ commands/fun/bj.js
@@ -125,6 +125,53 @@ module.exports = {
                             stand.stop()
                         }
                     })
+                    hit.on("collect", r => {
+                        let playercard3 = Math.floor(Math.random()*11) + 2;
+                        let botcard3 = Math.floor(Math.random()*11) + 2;
+                        playerArray.push(playercard3)
+                        botArray.push(botcard3)
+                        let sum2 = botArray.reduce(function(a, b){
+                            return a + b;
+                        }, 0);
+                        let botcount;
+                        botcount = sum2;
+    
+                        let sum = playerArray.reduce(function(a, b){
+                            return a + b;
+                        }, 0);
+                        let playercount;
+                        playercount = sum;
+
+                        if(playercount >= 21 && botcount >= 21){
+                            embed.setDescription(`Bot\'s Cards: ${botArray}: ${botcount}\nYour Cards: ${playerArray}: ${playercount}\nI won!`)
+                            .setColor("RED")
+                            msg.edit(embed)
+                            hit.stop()
+                            stand.stop()
+                        }else if(playercount === 21 && botcount < 21) {
+                            embed.setDescription(`Bot\'s Cards: ${botArray}: ${botcount}\nYour Cards: ${playerArray}: ${playercount}\nYou won!`)
+                            .setColor("GREEN")
+                            msg.edit(embed)
+                            hit.stop()
+                            stand.stop()
+                        }else if(botcount === 21 && playercount < 21) {
+                            embed.setDescription(`Bot\'s Cards: ${botArray}: ${botcount}\nYour Cards: ${playerArray}: ${playercount}\nI won!`)
+                            .setColor("RED")
+                            msg.edit(embed)
+                            hit.stop()
+                            stand.stop()
+                        }else if(playercount === 21 && botcount > 21) {
+                            embed.setDescription(`Bot\'s Cards: ${botArray}: ${botcount}\nYour Cards: ${playerArray}: ${playercount}\nYou won!`)
+                            .setColor("GREEN")
+                            msg.edit(embed)
+                            hit.stop()
+                            stand.stop()
+                        }else{
+                            embed.setDescription(`Bot\'s Cards: ${botcard}, ?? Total: ??\nYour Cards: ${playerArray}: ${playercount}`)
+                            embed.setColor(0x4AEFBA)
+                            msg.edit(embed);
+                        }
+                    })
                 });
             });
         }
