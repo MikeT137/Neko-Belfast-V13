d09f6f70db90cf6ca792da2559f283eda3ce4f73
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-08-10 21:26:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,20 +19,6 @@ bot.on('ready', () => {
     bot.user.setActivity('Use b!help for commands', {type: 'PLAYING'}).catch(console.error);
 });
 
-//Welcome Command
-bot.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.cache.find(channel => channel.name === 'welcome' || channel.name === 'Welcome' || channel.name === 'welcome-goodbye' || channel.name === 'Welcome-Goodbye' || channel.name === '👋┊welcome-goodbye' || channel.name === '👋┊Welcome-Goodbye')
-
-    channel.send(`Welcome ${member} to the server! Before doing anything, please read the rules of the server and...enjoy your stay ;3`)
-})
-
-//Leaving Command
-bot.on('guildMemberRemove', member => {
-    const channel = member.guild.channels.cache.find(channel => channel.name === 'welcome' || channel.name === 'Welcome' || channel.name === 'welcome-goodbye' || channel.name === 'Welcome-Goodbye' || channel.name === '👋┊welcome-goodbye' || channel.name === '👋┊Welcome-Goodbye')
-
-    channel.send(`${member} left the server, oof`)
-})
-
 //Commands
 bot.on('message', async message => {
     if(message.author.bot) return;
