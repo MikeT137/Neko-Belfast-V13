45ee99fff9b77cca12b8f856962b90f30da91f63
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-08-19 21:31:22 +0300

    Update say.js

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'say',
     description: 'The bot says what you want',
     run: async (bot, message, args) => {
-        const say_msg = args[0];
+        const say_msg = args.length >= -1;
         if(!say_msg){
             message.channel.send('You have to give me something to say')
         }else{
