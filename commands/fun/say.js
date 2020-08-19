f27d76220524621c755fc7b03e00e3c3b80b9ffa
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-08-19 21:36:10 +0300

    Update say.js

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -2,10 +2,10 @@ module.exports = {
     name: 'say',
     description: 'The bot says what you want',
     run: async (bot, message, args) => {
-        const say_msg = args.length >= -1;
-        if(!say_msg){
+        const say_msg = args;
+        if(!args[0]){
             message.channel.send('You have to give me something to say')
-        }else{
+        }else if(args[0]) {
             if(message.deletable) {
                 message.channel.send(`${say_msg}`);
                 message.delete();
