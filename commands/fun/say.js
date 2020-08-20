378be269561fdf3d7821afbdb0d3feb20a51941b
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-08-20 11:06:02 +0300

    Update say.js

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -6,11 +6,11 @@ module.exports = {
         if(!args[0]){
             message.channel.send('You have to give me something to say')
         }else if(args[0]) {
-            if(message.author.hasPermission("MANAGE_MESSAGES")) {
+            if(message.deletable)) {
                 message.channel.send(`${say_msg}`);
                 message.delete();
             }else {
-                message.channel.send('You dont have permission to use that command (permission needed: manage messages)')
+                message.channel.send('I cant say that because i cant delete messages')
             }
         }
     }
