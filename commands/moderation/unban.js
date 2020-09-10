d5dcaec6f14beac09b70fc8b34fde6f67f0383d9
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-09-10 18:23:33 +0300

    Update unban.js

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -1,21 +1,27 @@
 module.exports = {
     name:'unban',
-    description: "Unbans a user",
+    description: "Bans a user",
     run: async (bot, message, args) => {
         const member = message.mentions.members.first();
 
         if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send('You dont have permissions to use this command');
         
-        const bannedmember = await bot.fetchUser(args[0]);
+        const person = message.mentions.users.first();
 
-        if(!bannedmember) return message.channel.send('Please provide an id to unban someone!');
-        if(bannedmember) {
-            try{
-                message.guild.unban(bannedmember);
-                message.channel.send(`${bannedmember} has been unbanned`)
-            }catch(e) {
-                console.log(e.message)
+        if(person) {
+            const user = member.guild.member(person);
+
+            if(user) {
+                user.unban().then(() => {
+                    message.channel.send(`Successfullky unbanned ${person.tag}`);
+                }).catch(err => {
+                    message.channel.send('I was unnable to unban that member');
+                });
+            }else {
+                message.channel.send('That member isnt in the server')
             }
+        }else{
+            message.channel.send('Please specify a member');
         }
     }
 }
