9f46fb2fe2949d34d2ca709151e5c8c218696fba
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-09-18 23:55:36 +0300

    Update lovenick.js

--- commands/fun/lovenick.js
+++ commands/fun/lovenick.js
@@ -6,7 +6,7 @@ module.exports ={
         if(!args[0]) {
             const embed = new Discord.MessageEmbed()
 
-            .setTitle('Hello my lovely trap, i know i wasnt the best soulmate that you coudl\'ve had but, i still hope you cherrish this special command that i secretly made only for you, and thank you for accepting the way i am and for the awesome memories we\'ve had so far, I LOVE YOU <3')
+            .setTitle('Hello my lovely trap, i know i wasnt the best soulmate that you coudl\'ve had but, i still hope you cherrish this special command that i secretly made only for you, I LOVE YOU <3')
             .setImage('https://media.giphy.com/media/hnNyVPIXgLdle/giphy.gif')
             .setColor(0x4AEFBA)
             message.channel.send(embed);
