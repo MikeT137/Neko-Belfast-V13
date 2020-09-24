484973847da45fe2f5b6313c9a8dc4acbfd22b37
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-09-24 20:25:13 +0300

    Update server.js

--- commands/moderation/server.js
+++ commands/moderation/server.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'server',
     description: "Gives the user a link of the server of the bot",
     run: async (bot, message, args) => {
-        message.channel.send('Join my server please!! https://discord.gg/k2GuBMZ')
+        message.channel.send(':cupcake:Welcome to 𝕄𝕚𝕜𝕖\'𝕤 𝕄𝕒𝕟𝕤𝕚𝕠𝕟 ;3:cupcake:\nMike\'s Mansion is a anime community server where we just talk and relax, we expect everyone to get along in this server, that means:\n-No fighting\n-No arguing\n-No abusing\n-No bullying\n\nOn a more positive note, here at 𝕄𝕚𝕜𝕖\'𝕤 𝕄𝕒𝕟𝕤𝕚𝕠𝕟 we offer loads of cool stuff, for example:\n\n-Anime/manga talking\n-Games\n-Memes\n-100+ emojis\n-Bots\nAnd more!!\n\nPartnerships: If you\'d like to have a partnership with this server, please dm our admins or the owner!! Please be patient until we respond.:pikalove:\n\nThere are a lot of incredible people here to chat with and play games! If you\'d like to join this server, click on the link below! \nHave a great day cuties!\n\nServer link: https://discord.gg/xSkkeVf\nGif: https://giphy.com/gifs/black-and-white-girl-kawaii-W9MrfVxE4s2Zi')
     }
 }
