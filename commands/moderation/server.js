acfadbab1fe5370937a56620a86cc9cb6832b277
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-09-24 19:59:26 +0300

    Update server.js

--- commands/moderation/server.js
+++ commands/moderation/server.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'server',
     description: "Gives the user a link of the server of the bot",
     run: async (bot, message, args) => {
-        message.channel.send('Join my server please!! https://discord.gg/pf2bnUd')
+        message.channel.send('Join my server please!! https://discord.gg/k2GuBMZ')
     }
 }
