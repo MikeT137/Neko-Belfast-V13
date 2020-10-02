64c9711c9366516641742d53d8cef68e787fd505
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-10-02 22:00:55 +0300

    Update storyrun.js

--- commands/fun/storyrun.js
+++ commands/fun/storyrun.js
@@ -3,8 +3,10 @@ module.exports = {
     description: "It tells the story of a helpless boy (RUN)",
     run: async (bot, message, args) => {
         message.channel.send('*You\'re a 20 year old boy. You live in a small cabin inside a forest. You\'re there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:\n👍 - still chop wood\n✊ - go inside your house\n👎 - take your axe with you and investigate the zone*\n\n**Select your choice:**')
-        message.react('👍'),
-        message.react('✊'),
-        message.react('👎');
+        .then(msg => {
+        msg.react('👍'),
+        msg.react('✊'),
+        msg.react('👎');
+        })
     }
 }
