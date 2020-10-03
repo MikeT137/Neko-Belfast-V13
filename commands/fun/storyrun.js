50e606a813c331f090bf69938e7e58616df38116
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-10-03 11:26:18 +0300

    Update storyrun.js

--- commands/fun/storyrun.js
+++ commands/fun/storyrun.js
@@ -2,7 +2,7 @@ module.exports = {
     name:'storyrun',
     description: "It tells the story of a helpless boy (RUN)",
     run: async (bot, message, args) => {
-        message.channel.send('*You\'re a 20 year old boy. You live in a small cabin inside a forest. You\'re there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:\n\n👍 - still chop wood\n✊ - go inside your house\n👎 - take your axe with you and investigate the zone*\n\n**Select your choice:**')
+        message.channel.send('**Story - RUN**\n*You\'re a 20 year old boy. You live in a small cabin inside a forest. You\'re there alone, no parents, no friends, no one. Its currently 17th of october. You thought that it would be really good if you chopped some wood with an axe so you can warm yourself when winter comes. You chop wood, doing your business. But suddenly you hear a weird sound coming from afar. In that moment you:\n\n👍 - still chop wood\n✊ - go inside your house\n👎 - take your axe with you and investigate the zone*\n\n**Select your choice:**')
         .then(msg => {
         msg.react('👍'),
         msg.react('✊'),
@@ -16,7 +16,7 @@ module.exports = {
             .then(collected => {
                 const reaction = collected.first();
                 if(reaction.emoji.name === '👍') {
-                    msg.edit('**Story - RUN**\n*You think to yourself that it was just an animal or something, so you decide to ignore it. You then finish chopping the wood, you grab all of it, and go towards your basement so you can leave it there. But then you hear that weird sound again, this time coming from your basement. You then:\n\n👍 - go in the basement\n👎 - leave the cabin*\n\n**Select your choice:**')
+                    msg.edit('*You think to yourself that it was just an animal or something, so you decide to ignore it. You then finish chopping the wood, you grab all of it, and go towards your basement so you can leave it there. But then you hear that weird sound again, this time coming from your basement. You then:\n\n👍 - go in the basement\n👎 - leave the cabin*\n\n**Select your choice:**')
 
                     msg.awaitReactions(filter, { max: 1 })
                     .then(collected => {
