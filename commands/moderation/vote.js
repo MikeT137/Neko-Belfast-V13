4d4af471f1702af0931055f0e872f77b650280cb
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-10-04 22:36:32 +0300

    Update vote.js

--- commands/moderation/vote.js
+++ commands/moderation/vote.js
@@ -1,8 +1,15 @@
 module.exports = {
     name: 'vote',
-    description: 'The user can vote the bot in top.gg bot list',
+    description: 'The user can vote the bot or server in top.gg site',
     run: async(bot, message, args) => {
-        const votelink = 'https://top.gg/bot/727093236954431488/vote';
-        message.channel.send(`You can vote for this bot here: ${votelink}`)
+        const votebot = 'https://top.gg/bot/727093236954431488/vote';
+        const voteserver = 'https://top.gg/servers/597109214707580940/vote';
+        if(!args[0]) {
+            message.channel.send('Tell me what vote link you want, \`bot\` or \`server\` ?')
+        } else if(args[0] == 'bot') {
+            message.channel.send(`You can vote for this bot here: ${votebot}`)
+        } else if(args[0] == 'server') {
+            message.channel.send(`You can vote for my server here: ${voteserver}`)
+        }
     }
 }
