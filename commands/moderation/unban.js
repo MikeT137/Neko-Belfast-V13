71b5144f6c2ce4553fdabfe90631bc27339ee82f
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-10-06 16:29:04 +0300

    Create unban.js

---
+++ commands/moderation/unban.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'unban',
+    description: 'A caommand that an admin uses to unban users',
+    run: async(bot, message, args) => {
+        if(!message.member.hasPermission("BAN_MEMBERS")) {
+            return message.channel.send(`You do not have the permission to unban someone`)
+        } else if(!message.guild.me.hasPermission("BAN_MEMBERS")) {
+            return message.channel.send(`I do not have the permission to unban someone`)
+        }
+          
+        let userID = args[0]
+        message.guild.fetchBans()
+        .then(bans => {
+            if(bans.size == 0) return 
+            let bUser = bans.find(b => b.user.id == userID)
+            if(!bUser) return
+            message.guild.members.unban(bUser.user)
+        })
+    }
+}
