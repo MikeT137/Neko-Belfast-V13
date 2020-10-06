870e350b862cb9616edee395d639cf995df8fbe2
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-10-06 21:35:46 +0300

    Update random.js

--- commands/fun/random.js
+++ commands/fun/random.js
@@ -4,8 +4,10 @@ module.exports = {
     run: async (bot, message, args) => {
         if(!args[0]){
             message.channel.send('You have to specify a number/limit of the random number')
-        }else if(args[0]) {
+        }else if(args[0] == '1') {
+            message.channel.send('Your maximum value must be at least higher than 1')
+        }else if(args[0] < '1') {
             message.channel.send(`Your random number is: ${Math.floor(Math.random() * Math.floor(args[0]))}`)
-        }
+        }else return;
     }
 }
