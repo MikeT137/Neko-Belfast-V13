db573f26132ea737a5e152da718161c6d4fe1d90
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-10-08 19:08:17 +0300

    Update rng.js

--- commands/fun/rng.js
+++ commands/fun/rng.js
@@ -5,7 +5,7 @@ module.exports = {
         if(!args[0]){
             message.channel.send('You have to specify a number/limit of the random number')
         }else if(args[0]) {
-            message.channel.send(`Your random number is: ${Math.floor(Math.random() * Math.floor(args[0]))}`)
+            message.channel.send(`Your random number is: ${Math.floor(Math.random() * Math.floor(args[0]))+1}`)
         }else if (args[1]) {
             message.channel.send('Please dont add any other arguments other than your number')
         }
