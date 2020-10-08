fd42756ee3e024c8cb74f26acda505ed435900e9
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-10-08 19:19:20 +0300

    dfnhg

--- commands/fun/rng.js
+++ commands/fun/rng.js
@@ -2,12 +2,14 @@ module.exports = {
     name: 'rng',
     description: 'It gives a random number',
     run: async (bot, message, args) => {
-        if(!args[0]){
-            message.channel.send('You have to specify a number/limit of the random number')
-        }else if(args[0]) {
+        if(!args[0]) {
+            message.channel.send('You have to specify a number/limit of the random number (has to be higher than 1, or lower than -1')
+        }else if(args[0] > '1' || args[0] < '-1') {
             message.channel.send(`Your random number is: ${Math.floor(Math.random() * Math.floor(args[0]))+1}`)
         }else if (args[1]) {
             message.channel.send('Please dont add any other arguments other than your number')
+        }else {
+            return;
         }
     }
 }
