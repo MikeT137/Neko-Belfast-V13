bc54353972d2bc2c1a3d1fcb8f23a356f6faac94
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-10-12 20:00:27 +0300

    Update ttt.js

--- commands/fun/ttt.js
+++ commands/fun/ttt.js
@@ -7,17 +7,12 @@ module.exports = {
         if(args[0]) {
             return;
         }else if (!args[0]) {
-            const ttt = require("tictactoe")
-            const prefix = "!"
-            const embed_color = "0x4AEFBA"
-            const start_cmd = "ttt"
-            ttt.run(bot, prefix, embed_color, start_cmd)
-            /*const embed = new Discord.MessageEmbed()
+            const embed = new Discord.MessageEmbed()
 
             .setTitle('Tic Tac Toe')
             .addField(`${message.author.username} - :regional_indicator_x: **VS** NekoBelfast - :regional_indicator_o:`, ':blue_square::blue_square::blue_square:\n:blue_square::blue_square::blue_square:\n:blue_square::blue_square::blue_square:')
             .setColor(0x4AEFBA)
-            message.channel.send(embed);*/
+            message.channel.send(embed);
         }
     }
 }
