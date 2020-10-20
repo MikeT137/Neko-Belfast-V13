2d43680f5b740e9371512273f923a6994dfe013b
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-10-20 20:26:03 +0300

    Create choc.js

---
+++ commands/fun/choc.js
@@ -0,0 +1,7 @@
+/*module.exports = {
+    name: 'choc',
+    description: 'Lovely gf choc',
+    run: async (bot, message, args) => {
+        message.channel.send('Hey Choc :3\nIm really grateful for accepting me in your life, and i love the good memories we\'ve had so far, and i cant wait to make even more god memories\nI LOVE YOU <3')
+    }
+}*/
