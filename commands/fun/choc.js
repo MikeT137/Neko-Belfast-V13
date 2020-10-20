faee9bdf483d3aea9f0f5861450202a56bfc1adb
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-10-20 20:27:59 +0300

    Update choc.js

--- commands/fun/choc.js
+++ commands/fun/choc.js
@@ -1,7 +1,27 @@
-/*module.exports = {
+module.exports = {
     name: 'choc',
     description: 'Lovely gf choc',
     run: async (bot, message, args) => {
-        message.channel.send('Hey Choc :3\nIm really grateful for accepting me in your life, and i love the good memories we\'ve had so far, and i cant wait to make even more god memories\nI LOVE YOU <3')
+        const Discord = require('discord.js');
+        const random_kiss = [
+            'https://media.giphy.com/media/MQVpBqASxSlFu/giphy.gif',
+            'https://media.giphy.com/media/G3va31oEEnIkM/giphy.gif',
+            'https://media.giphy.com/media/hnNyVPIXgLdle/giphy.gif',
+            'https://media.giphy.com/media/11rWoZNpAKw8w/giphy.gif',
+            'https://media.giphy.com/media/jR22gdcPiOLaE/giphy.gif',
+            'https://media.giphy.com/media/flmwfIpFVrSKI/giphy.gif',
+            'https://acegif.com/wp-content/uploads/anime-kiss-m.gif',
+            'https://thumbs.gfycat.com/WarpedSlightFrigatebird-size_restricted.gif',
+            'https://media.giphy.com/media/hnNyVPIXgLdle/giphy.gif',
+            'https://i.gifer.com/QPB7.gif',
+        ]
+        if(!args[0]) {
+            const embed = new Discord.MessageEmbed()
+
+            .setAuthor('Hey Choc :3 Im really grateful for accepting me in your life, and i love the good memories we\'ve had so far, and i cant wait to make even more god memoriesI LOVE YOU <3')
+            .setImage(random_kiss[Math.floor(Math.random() * random_kiss.length)])
+            .setColor(0x4AEFBA)
+            message.channel.send(embed);
+        }
     }
-}*/
+}
