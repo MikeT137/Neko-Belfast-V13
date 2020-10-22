c89f4d77324aa54ab2d16dd17f42bd99dc46dc90
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-10-22 21:55:08 +0300

    Update coinflip.js

--- commands/fun/coinflip.js
+++ commands/fun/coinflip.js
@@ -2,7 +2,11 @@ module.exports = {
     name: 'coinflip',
     description: 'The user flips a coin',
     run: async (bot, message, args) => {
-        const coinface = ['Heads!', 'Tails!', 'OMG it landed on the edge! Amazing!']
+        const coinface = [
+            'Heads! :coin:',
+            'OMG it landed on the edge! Amazing! :coin:',
+            'Tails! :coin:'
+        ]
         const answer = coinface[Math.floor(Math.random() * coinface.length)];
 
         if(!args[0]){
