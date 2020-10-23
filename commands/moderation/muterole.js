6325af62a13aaf688e2189366f534aa4897011b3
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-10-23 17:14:02 +0300

    Update muterole.js

--- commands/moderation/muterole.js
+++ commands/moderation/muterole.js
@@ -9,6 +9,7 @@ module.exports = {
                 data: {
                     name: 'BMuted',
                     color: 'RED',
+                    permissions: [{CREATE_INVITE: false}, {SEND_MESSAGES: false}, {EMBED_LINKS: false}, {ATTACH_FILES: false}, {USE_EXTERNAL_EMOJIS: false}, {ADD_REACTIONS: false}]
                 }
             })
 
