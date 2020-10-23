5939e5e1952dc22bf340f70cc67b4288a72ed6dc
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-10-23 17:42:11 +0300

    Create ttt.js

---
+++ commands/fun/ttt.js
@@ -0,0 +1,15 @@
+/*module.exports = {
+    name: 'ttt',
+    description: 'The user plays tic tac toe',
+    run: async(bot, message, args) => {
+        const Discord = require('discord.js')
+
+        if(!args[0]) {
+            const embed = new Discord.MessageEmbed()
+
+            .setTitle('Tic Tac Toe')
+            .addField(`${message.author.username}(:regional_indicator_x:) VS Neko Belfast(:regional_indicator_o:)`, ':blue_square::blue_square::blue_square:\n:blue_square::blue_square::blue_square:\n:blue_square::blue_square::blue_square:')
+            message.channel.send(embed)
+        }else return;
+    }
+}*/
