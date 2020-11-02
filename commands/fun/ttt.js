7622048f64b05b8f653824960f58fb53f92fe9c7
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-11-02 15:40:45 +0200

    ftgh

--- commands/fun/ttt.js
+++ commands/fun/ttt.js
@@ -1,15 +1,20 @@
-/*module.exports = {
+module.exports = {
     name: 'ttt',
     description: 'The user plays tic tac toe',
     run: async(bot, message, args) => {
         const Discord = require('discord.js')
 
         if(!args[0]) {
-            const embed = new Discord.MessageEmbed()
+            const ttt = require("discord.js-tictactoe")
+            const embed_color = "0x4AEFBA"
+            const start_cmd = "ttt"
+            ttt.run(bot, prefix, embed_color, start_cmd)
+            /*const embed = new Discord.MessageEmbed()
 
             .setTitle('Tic Tac Toe')
             .addField(`${message.author.username}(:regional_indicator_x:) VS Neko Belfast(:regional_indicator_o:)`, ':blue_square::blue_square::blue_square:\n:blue_square::blue_square::blue_square:\n:blue_square::blue_square::blue_square:')
-            message.channel.send(embed)
+            .setColor(0x4AEFBA)
+            message.channel.send(embed)*/
         }else return;
     }
-}*/
+}
