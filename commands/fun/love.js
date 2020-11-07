e894d98461dfcda1db90577a937f8593df5ef941
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-11-07 21:19:08 +0200

    jyrtjskyjk

--- commands/fun/love.js
+++ commands/fun/love.js
@@ -2,8 +2,8 @@ module.exports = {
     name:'love',
     description: "The bot gives a nice message and snuggles the user",
     run: async (bot, message, args) => {
-        const author = message.author;
         const Discord = require('discord.js');
+        const person = message.mentions.users.first();
         const random_snuggle = [
             'https://media.giphy.com/media/C4gbG94zAjyYE/giphy.gif',
             'https://media.giphy.com/media/BXrwTdoho6hkQ/giphy.gif',
@@ -19,10 +19,17 @@ module.exports = {
         if (!args[0]){
             const embed = new Discord.MessageEmbed()
 
-            .setAuthor(`Nyaa~, i love you ${author.username}`, message.author.displayAvatarURL({ dynamic: true }))
-            .setImage(random_snuggle[Math.floor(Math.random() * random_snuggle.length)])
-            .setColor(0x4AEFBA)
+                .setAuthor(`Nyaa~, i love you ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
+                .setImage(random_snuggle[Math.floor(Math.random() * random_snuggle.length)])
+                .setColor(0x4AEFBA)
             message.channel.send(embed);
-        }
+        }else if (args[0] || message.mentions.members.size >= 1) {
+            const embed = new Discord.MessageEmbed()
+
+                .setAuthor(`${message.author.username} loves you ${person.username}!`, message.author.displayAvatarURL({ dynamic: true }))
+                .setImage(random_snuggle[Math.floor(Math.random() * random_snuggle.length)])
+                .setColor(0x4AEFBA)
+            message.channel.send(embed);
+        }else return;
     }
 }
