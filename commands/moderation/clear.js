4f6c4a64c9648a0090a0648f690c8593723b99da
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-11-18 23:17:12 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -4,22 +4,24 @@ module.exports = {
     run: async (bot, message, args) => {
         let amount;
 
-        if(message.deletable) {
-            message.delete();
-        }else if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+        /*if(message.deletable) {
+            message.delete();*/
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) {
             message.channel.send('You cant delete messages')
         }else if(isNaN(args[0] || parseInt(args[0])) <= 0) {
             message.channel.send('You didn\'t tell me how many messages to delete')
         }else if(!message.guild.me.hasPermission("MANAGE_MESSAGES ")) {
             message.channel.send('I can\'t delete messages')
-        }else if(parseInt(args[0]) > 100) {
-            amount = 100;
         }else {
-            amount = parseInt(args[0]);
+            if(parseInt(args[0]) > 100) {
+                amount = 100;
+            }else {
+                amount = parseInt(args[0]);
+            }
+            message.channel.bulkDelete(amount, true)
+            .then(deleted => message.channel.send(`I deleted \`${deleted.size}\` messages`))
+            .catch(err => message.channel.send(`Something went wrong...\n${err}`));
         }
-        message.channel.bulkDelete(amount, true)
-        .then(deleted => message.channel.send(`I deleted \`${deleted.size}\` messages`))
-        .catch(err => message.channel.send(`Something went wrong...\n${err}`));
 
     }
 }
