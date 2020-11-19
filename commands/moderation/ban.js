d48b6ce0a173c29293667562cf05786f5d4ed1b0
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-11-19 15:17:44 +0200

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -5,15 +5,13 @@ module.exports = {
         const member = message.mentions.users.first();
 
         if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send('You dont have permissions to use this command');
-        
-        const person = message.mentions.users.first();
 
-        if(person) {
-            const user = message.guild.member(person);
+        if(member) {
+            const user = message.guild.member(member);
 
             if(user) {
                 user.ban().then(() => {
-                    message.channel.send(`Successfullky banned ${person.tag}`);
+                    message.channel.send(`Successfullky banned ${member.tag}`);
                 }).catch(err => {
                     message.channel.send('I was unnable to ban that member');
                 });
