53fb6886f74d66e1688bb63930aa33ae5d139fcf
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-11-19 15:29:06 +0200

    Update kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -2,20 +2,16 @@ module.exports = {
     name:'kick',
     description: "Kicks a user",
     run: async (bot, message, args) => {
-        const member = message.mentions.members.first();
+        const member = message.mentions.users.first();
 
-        if(!message.member.hasPermission("MANAGE_ROLES") || !message.guild.owner) return message.channel.send('You dont have permissions to use this command');
+        if(!message.member.hasPermission(["KICK_MEMBERS", "ADMINISTRATOR"])) return message.channel.send('You dont have permissions to use this command');
 
-        const person = message.mentions.users.first();
-
-        if(person) {
-            const user = member.guild.member(person);
+        if(member) {
+            const user = message.guild.member(member);
 
             if(user) {
                 user.kick().then(() => {
-                    message.channel.send(`Successfullky kicked ${person.tag}`);
-                }).catch(err => {
-                    message.channel.send('I was unnable to kick that member');
+                    message.channel.send(`Successfully kicked ${member.tag}`);
                 });
             }else {
                 message.channel.send('That member isnt in the server')
