acae5fb09325f172d338b718a950c869888550dd
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-11-26 13:02:41 +0200

    Create suggest.js

---
+++ commands/moderation/suggest.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'suggest',
+    description: "It sends the creator a suggestion",
+    run: async (bot, message, args) => {
+        if(!args[0]) {
+            message.channel.send('You have to say something to suggest it to the creator')
+        }else if(args[0]) {
+            message.author.bot.send(`${args[1]}`)
+        }else return;
+    }
+}
