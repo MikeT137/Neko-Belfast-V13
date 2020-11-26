622d48c11426f1a96e2d69d5fb8bf4f41a8a950c
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-11-26 13:25:54 +0200

    Update suggest.js

--- commands/moderation/suggest.js
+++ commands/moderation/suggest.js
@@ -2,10 +2,12 @@ module.exports = {
     name: 'suggest',
     description: "It sends the creator a suggestion",
     run: async (bot, message, args) => {
+        const miku = 'Miku Yoruka#6107';
+
         if(!args[0]) {
             message.channel.send('You have to say something to suggest it to the creator')
         }else if(args[0]) {
-            message.member.user.send(`${args[0]}`)
+            message.miku.send(`${args[0]}`)
         }else return;
     }
 }
