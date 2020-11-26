9ac0c23788c0c429c7711e65a59c66321f52ead6
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-11-26 13:45:37 +0200

    Update suggest.js

--- commands/moderation/suggest.js
+++ commands/moderation/suggest.js
@@ -6,7 +6,7 @@ module.exports = {
         if(!args[0]) {
             message.channel.send('You have to say something to suggest it to the creator')
         }else if(args[0]) {
-            message.bot.send(`${args[0]}`)
+            MikuYoruka.send(`${args[0]}`)
         }else return;
     }
 }
