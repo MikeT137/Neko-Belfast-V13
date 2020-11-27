64978476472307aef6e4a87d2881ff6a0c211815
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-11-27 23:13:17 +0200

    Update donate.js

--- commands/moderation/donate.js
+++ commands/moderation/donate.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "It sends a link of the patreon page",
     run: async (bot, message, args) => {
         if(!args[0]) {
-            message.channel.send('Wanna support me? Donate here to become a patreon nya~\n')
+            message.channel.send('Wanna support me? Donate here to become a patreon nya~\n') //link
         }else return;
     }
 }
