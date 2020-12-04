a37b0a9b3fd65a4de3690f83aeaeed4029058645
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-12-04 23:29:25 +0200

    gyhbjy

--- events/client/ready.js
+++ events/client/ready.js
@@ -5,4 +5,17 @@ module.exports = bot => {
     setInterval(function(){
         bot.user.setActivity(`${bot.guilds.cache.size} servers | use b.help for commands`, {type: 'WATCHING'}).catch(console.error);
     }, 10000)
+
+    //Discord Bot List - Server Count
+    const DBL = require("dblapi.js");
+    const topgglink = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcyNzA5MzIzNjk1NDQzMTQ4OCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjAxMDIxNzQ2fQ.QCfXPZhovBGFszhJAtrdBTChuh92-xrtGRoGiCJnYnU';
+    const dbl = new DBL(`${topgglink}`, bot);
+
+    dbl.on('posted', () => {
+    console.log('Server count posted!');
+    })
+
+    dbl.on('error', e => {
+    console.log(`Oops! ${e}`);
+    });
 }
