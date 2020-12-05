b875bac3e88a2bcffb877e576fc082b2512a4d27
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-12-05 21:40:55 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -7,7 +7,7 @@ module.exports = bot => {
         bot.user.setActivity(`${bot.guilds.cache.size} servers | use b.help for commands`, {type: 'WATCHING'}).catch(console.error);
     }, 10000)
 
-    //Fixed dates
+    //Holidays
     const newyear = schedule.scheduleJob({month:1, date:1}, function(){
         bot.user.setActivity(`Happy New Year nya~!`, {type: 'WATCHING'}).catch(console.error)
     });
@@ -16,6 +16,15 @@ module.exports = bot => {
     });
     const patrick = schedule.scheduleJob({month:3, date:17}, function(){
         bot.user.setActivity(`Happy St. Patrick's Day nya~!`, {type: 'WATCHING'}).catch(console.error)
+    });
+    const easter = schedule.scheduleJob({month:4, date: 8}, function(){
+        bot.user.setActivity(`Happy Easter nya~!`, {type: 'WATCHING'}).catch(console.error)
+    });
+    const mother = schedule.scheduleJob({month:5, date:11}, function(){
+        bot.user.setActivity(`Happy Mother's Day nya~!`, {type: 'WATCHING'}).catch(console.error)
+    });
+    const father = schedule.scheduleJob({month:6, date:18}, function(){
+        bot.user.setActivity(`Happy Father's Day nya~!`, {type: 'WATCHING'}).catch(console.error)
     });
     const belfast = schedule.scheduleJob({month:6, date:29}, function(){
         bot.user.setActivity(`It's my birthday nya~!`, {type: 'WATCHING'}).catch(console.error)
@@ -26,21 +35,10 @@ module.exports = bot => {
     const halloween = schedule.scheduleJob({month:10, date:31}, function(){
         bot.user.setActivity(`Have a spooky Halloween! Tee hee`, {type: 'WATCHING'}).catch(console.error)
     });
-    const xmas = schedule.scheduleJob({month:10, date:31}, function(){
-        bot.user.setActivity(`Merry Christmas nya!`, {type: 'WATCHING'}).catch(console.error)
-    });
-
-    //Floating dates
-    const easter = schedule.scheduleJob({month:4, date: 4}, function(){
-        bot.user.setActivity(`Happy Easter nya~!`, {type: 'WATCHING'}).catch(console.error)
-    });
-    const mother = schedule.scheduleJob({month:5, date:2}, function(){
-        bot.user.setActivity(`Happy Mother's Day nya~!`, {type: 'WATCHING'}).catch(console.error)
-    });
-    const father = schedule.scheduleJob({month:6, date:20}, function(){
-        bot.user.setActivity(`Happy Father's Day nya~!`, {type: 'WATCHING'}).catch(console.error)
-    });
     const thanks = schedule.scheduleJob({month:11, date:25}, function(){
         bot.user.setActivity(`Happy Thanksgiving nya~!`, {type: 'WATCHING'}).catch(console.error)
+    });
+    const xmas = schedule.scheduleJob({month:12, date:25}, function(){
+        bot.user.setActivity(`Merry Christmas nya!`, {type: 'WATCHING'}).catch(console.error)
     });
 }
