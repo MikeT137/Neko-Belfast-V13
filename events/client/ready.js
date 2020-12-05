d528ea2032bc1a652288445d9e3caa225a63ddf7
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-12-05 20:24:05 +0200

    sdfg

--- events/client/ready.js
+++ events/client/ready.js
@@ -5,4 +5,9 @@ module.exports = bot => {
     setInterval(function(){
         bot.user.setActivity(`${bot.guilds.cache.size} servers | use b.help for commands`, {type: 'WATCHING'}).catch(console.error);
     }, 10000)
+    var schedule = require('node-schedule');
+
+    var j = schedule.scheduleJob('26 * * * *', function(){
+        bot.user.setActivity(`awoo`).catch(console.error)
+    });
 }
