aacbbf587473c759674f3e1c862eecb03806ee08
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-12-08 21:40:27 +0200

    Update botinfo.js

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -4,8 +4,8 @@ module.exports = {
     run: async (bot, message, args) => {
         const Discord = require('discord.js');
         const moment = require('moment');
-        const version = require('../../package.json');
-        const author = require('../../package.json');
+        const {version} = require('../../package.json');
+        const {author} = require('../../package.json');
         //'Im Belfast:heart:, your lovely neko maid that will be happy to serve you in any way you want, but have patience with me since i learn new things very slowly nya~')
 
         if (!args[0]){
