a6a8a56c1b311b0a5cef0b8da8531ad08f77dafd
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-12-11 13:28:20 +0200

    hfdg

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,11 +1,24 @@
 const {prefix} = require ('../../config.json')
 
 module.exports = bot => {
+    const DBL = require("dblapi.js");
+    const topgglink = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcyNzA5MzIzNjk1NDQzMTQ4OCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjAxMDIxNzQ2fQ.QCfXPZhovBGFszhJAtrdBTChuh92-xrtGRoGiCJnYnU';
+    const dbl = new DBL(`${topgglink}`, bot);
+    
+    dbl.on('posted', () => {
+    console.log('Server count posted!');
+    })
+    
+    dbl.on('error', e => {
+    console.log(`Oops! ${e}`);
+    });
+
     console.log('Neko Belfast is online!');
     setInterval(function(){
         bot.user.setActivity(`${bot.guilds.cache.size} servers | use ${prefix}help for commands`, {type: 'WATCHING'}).catch(console.error);
     }, 10000)
 
+    
     /*Holidays
     January 1st - `Happy New Year nya~!`
     February 14th - `Will you be my Valentine nya~?`
