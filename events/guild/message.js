67d186c27bf62ad4e5f73135072a821d49173e91
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-12-11 15:52:45 +0200

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -8,7 +8,7 @@ module.exports = async(bot, message) => {
 
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
-    var command = bot.commands.get(cmd);
+    let command = bot.commands.get(cmd);
 
     if(cmd.length == 0) return;
     if(!command) command = bot.commands.get(bot.aliases.get(cmd));
