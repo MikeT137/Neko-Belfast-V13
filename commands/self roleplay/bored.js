d3f5df4f10ab61c4a61e8035acc2e8ff1b254262
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-12-19 17:14:49 +0200

    Update bored.js

--- commands/self roleplay/bored.js
+++ commands/self roleplay/bored.js
@@ -6,7 +6,7 @@ module.exports = {
         const Discord = require('discord.js');
         const random_bored = [
             'https://www.pinterest.at/pin/808325833102707555/?amp_client_id=CLIENT_ID(_)&mweb_unauth_id={{default.session}}&amp_url=https%3A%2F%2Fwww.pinterest.at%2Famp%2Fpin%2F808325833102707555%2F&from_amp_pin_page=true',
-            'https://tenor.com/view/bored-anime-boy-cute-mood-gif-16728079',
+            'https://media1.tenor.com/images/4b84b1b5b40033e0a3a0bb2e22b7230d/tenor.gif?itemid=16728079',
             'https://ro.pinterest.com/pin/813392382687675938/',
             'https://tenor.com/view/anime-boring-bored-whatever-so-what-gif-8118376',
             'https://www.pinterest.pt/pin/53832158023010882/?send=true',
@@ -19,7 +19,7 @@ module.exports = {
         if (!args[0]){
             const embed = new Discord.MessageEmbed()
 
-            .setAuthor(`${author.username} is blushing`, message.author.displayAvatarURL({ dynamic: true }))
+            .setAuthor(`${author.username} is bored`, message.author.displayAvatarURL({ dynamic: true }))
             .setImage(random_bored[Math.floor(Math.random() * random_bored.length)])
             .setColor(0x4AEFBA)
             message.channel.send(embed);
