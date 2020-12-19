21e56543ddb856fd790e8406af59e80402fe9fd1
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-12-19 17:30:11 +0200

    Update bored.js

--- commands/self roleplay/bored.js
+++ commands/self roleplay/bored.js
@@ -5,16 +5,16 @@ module.exports = {
         const author = message.author;
         const Discord = require('discord.js');
         const random_bored = [
-            'https://www.pinterest.at/pin/808325833102707555/?amp_client_id=CLIENT_ID(_)&mweb_unauth_id={{default.session}}&amp_url=https%3A%2F%2Fwww.pinterest.at%2Famp%2Fpin%2F808325833102707555%2F&from_amp_pin_page=true',
+            'https://i.pinimg.com/originals/40/ca/73/40ca73fab872f7a10f66f3dcafd09585.gif',
             'https://media1.tenor.com/images/4b84b1b5b40033e0a3a0bb2e22b7230d/tenor.gif?itemid=16728079',
-            'https://ro.pinterest.com/pin/813392382687675938/',
-            'https://tenor.com/view/anime-boring-bored-whatever-so-what-gif-8118376',
-            'https://www.pinterest.pt/pin/53832158023010882/?send=true',
-            'https://imgur.com/gallery/EzAWv47',
-            'https://picsart.com/i/176650305000202',
-            'https://reactionanime.blogspot.com/2014/01/bored-gif-reaction-anime.html',
-            'http://www.lowgif.com/view.html',
-            'https://gifer.com/en/Kntx'
+            'https://i.pinimg.com/originals/c3/a1/9c/c3a19cf3e79aab4ac4ea674e9764a5ab.gif',
+            'https://i.gifer.com/JHyT.gif',
+            'https://pa1.narvii.com/6061/fdc1d2f595cdcaa769177ec4151e9778847036c0_hq.gif',
+            'https://media1.tenor.com/images/0f0805c286205da154cce414aa110b98/tenor.gif?itemid=8118376',
+            'https://i.pinimg.com/originals/cd/2c/c6/cd2cc65ff07f62533821f952e3542bd8.gif',
+            'https://i.gifer.com/JPTs.gif',
+            'https://i.imgur.com/EzAWv47.gif',
+            'https://2.bp.blogspot.com/-RsPepQekbus/UsKt6g1gMDI/AAAAAAAACEY/78y2DEakcDo/s640/Bored+004.gif'
         ]
         if (!args[0]){
             const embed = new Discord.MessageEmbed()
