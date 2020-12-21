37f895ec8aeca30809bf0d58298469c0e54ace83
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-12-21 09:34:55 +0200

    ftgy

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,7 +1,5 @@
 module.exports = async(bot, message) => {
-    const fs = require('fs');
     const {prefix} = require ('../../config.json');
-    //const xp = require ('../../xp.json');
 
     if(message.author.bot) return;
     if(!message.content.toLowerCase().startsWith(prefix)) return;
@@ -15,23 +13,4 @@ module.exports = async(bot, message) => {
     if(cmd.length === 0) return;
     if(!command) command = bot.commands.get(bot.aliases.get(cmd));
     if(command) command.run(bot, message, args);
-
-    //XP System
-    /*const xpAdd = Math.floor(Math.random() * 7) + 8;
-    const currentxp = xp[message.author.id].xp;
-    const currentLvl = xp[message.author.id].level;
-    const nextLvl = xp[message.author.id].level * 250;
-
-    if(!xp[message.author.id]) {
-        xp[message.author.id] = {
-            xp: 0,
-            level: 1
-        }
-    }
-    xp[message.author.id].xp = currentxp + xpAdd;
-
-    if(nextLvl <= xp[message.author.id].xp) {
-        xp[message.author.id].level = currentLvl + 1;
-    }
-    fs.writeFile('../../xp.json', JSON.stringify(xp))*/
 }
