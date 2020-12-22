c097b82f59b5cc9d6ed8d37e136573ac4af881d7
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-12-22 21:28:53 +0200

    Update boop.js

--- commands/interractive roleplay/boop.js
+++ commands/interractive roleplay/boop.js
@@ -3,7 +3,9 @@ module.exports = {
     description: "Boops a user's nose",
     run: async (bot, message, args) => {
         const Discord = require('discord.js');
+        const randomPuppy = require('random-puppy');
         const person = message.mentions.users.first();
+        const img = await randomPuppy();
         /*const random_boop = [
             'https://media1.tenor.com/images/cbf38a2e97a348a621207c967a77628a/tenor.gif?itemid=6287077',
             'https://media.tenor.com/images/9945480efe5179c227558769613ee275/tenor.gif',
@@ -23,6 +25,7 @@ module.exports = {
                 const embed = new Discord.MessageEmbed()
         
                 .setAuthor(`${message.author.username} booped ${person.username}'s nose !`, message.author.displayAvatarURL({ dynamic: true }))
+                .setImage(img)
                 .setURL('https://tenor.com/search/anime-boop-gifs')
                 .setColor(0x4AEFBA)
                 message.channel.send(embed);
