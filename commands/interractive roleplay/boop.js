3be0e321adc65f4f20bb0b61007e5dc7b7eff80c
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-12-22 22:55:36 +0200

    Update boop.js

--- commands/interractive roleplay/boop.js
+++ commands/interractive roleplay/boop.js
@@ -4,7 +4,7 @@ module.exports = {
     run: async (bot, message, args) => {
         const {Collection, Client} = require('discord.js');
         const person = message.mentions.users.first();
-
+        /*
         // url Async requesting function
         function httpGetAsync(theUrl, callback)
         {
@@ -21,7 +21,7 @@ module.exports = {
             }
         
             // open as a GET call, pass in the url and set async = True
-            xmlHttp.get("GET", theUrl, true);
+            xmlHttp.open("GET", theUrl, true);
         
             // call send with no params as they were passed in on the url string
             xmlHttp.send(null);
@@ -73,7 +73,19 @@ module.exports = {
         
         // start the flow
         grab_data();
-
+        */
+       const random_boop = [
+            'https://media1.tenor.com/images/cbf38a2e97a348a621207c967a77628a/tenor.gif?itemid=6287077',
+            'https://media.tenor.com/images/9945480efe5179c227558769613ee275/tenor.gif',
+            'https://i.gifer.com/AfDr.gif',
+            'https://i.imgur.com/fZmUTgw.gif',
+            'https://media.tenor.com/images/75c40413d2a4ec935b707ef32ab53321/tenor.gif',
+            'https://66.media.tumblr.com/9a457a43bcae3ebaafda53d7fe6f572d/tumblr_pqjm6cCRt41th206io1_1280.gif',
+            'https://thumbs.gfycat.com/PowerlessEqualDeviltasmanian-size_restricted.gif',
+            'https://i.gifer.com/81H8.gif',
+            'https://steamuserimages-a.akamaihd.net/ugc/169289604394170554/4CAB1E72D3FCD5527A1A6BB4443AF5E9EC65627F/',
+            'https://media.tenor.com/images/d07762ab2f5fc5d1d43525d2b3db7de8/tenor.gif'
+        ]
         if(message.mentions.members.size >= 1) {
             if(person.id == message.author.id) {
                 message.channel.send('Eh? Why did you boop yourself?')
@@ -81,7 +93,7 @@ module.exports = {
                 const embed = new Client.MessageEmbed()
         
                 .setAuthor(`${message.author.username} booped ${person.username}'s nose !`, message.author.displayAvatarURL({ dynamic: true }))
-                .setImage()
+                .setImage(random_boop[Math.floor(Math.random() * random_boop.length)])
                 .setColor(0x4AEFBA)
                 message.channel.send(embed);
             }
