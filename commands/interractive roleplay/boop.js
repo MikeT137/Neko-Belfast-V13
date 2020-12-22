ab85b5dd8344adee003ae5b8c6bac58c4a3fb07f
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2020-12-22 22:13:34 +0200

    Update boop.js

--- commands/interractive roleplay/boop.js
+++ commands/interractive roleplay/boop.js
@@ -2,13 +2,80 @@ module.exports = {
     name:'boop',
     description: "Boops a user's nose",
     run: async (bot, message, args) => {
-        const Discord = require('discord.js');
-        const randomPuppy = require('random-puppy');
-        const person = message.mentions.users.first();
-        const boop = 'https://tenor.com/search/anime-boop-gifs';
-        const random = boop[Math.floor(Math.random() * boop.length)];
-        const img = await randomPuppy(random);
+
+        // url Async requesting function
+        function httpGetAsync(theUrl, callback)
+        {
+            // create the request object
+            var xmlHttp = new XMLHttpRequest();
         
+            // set the state change callback to capture when the response comes in
+            xmlHttp.onreadystatechange = function()
+            {
+                if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+                {
+                    callback(xmlHttp.responseText);
+                }
+            }
+        
+            // open as a GET call, pass in the url and set async = True
+            xmlHttp.open("GET", theUrl, true);
+        
+            // call send with no params as they were passed in on the url string
+            xmlHttp.send(null);
+        
+            return;
+        }
+        
+        // callback for the top 8 GIFs of search
+        function tenorCallback_search(responsetext)
+        {
+            // parse the json response
+            var response_objects = JSON.parse(responsetext);
+        
+            top_10_gifs = response_objects["results"];
+        
+            // load the GIFs -- for our example we will load the first GIFs preview size (nanogif) and share size (tinygif)
+        
+            document.getElementById("preview_gif").src = top_10_gifs[0]["media"][0]["nanogif"]["url"];
+        
+            document.getElementById("share_gif").src = top_10_gifs[0]["media"][0]["tinygif"]["url"];
+        
+            return;
+        
+        }
+        
+        
+        // function to call the trending and category endpoints
+        function grab_data()
+        {
+            // set the apikey and limit
+            var apikey = "LIVDSRZULELA";
+            var lmt = 8;
+        
+            // test search term
+            var search_term = "anime boop";
+        
+            // using default locale of en_US
+            var search_url = "https://api.tenor.com/v1/search?q=" + search_term + "&key=" +
+                    apikey + "&limit=" + lmt;
+        
+            httpGetAsync(search_url,tenorCallback_search);
+        
+            // data will be loaded by each call's callback
+            return;
+        }
+        
+        
+        // SUPPORT FUNCTIONS ABOVE
+        // MAIN BELOW
+        
+        // start the flow
+        grab_data();
+
+        const Discord = require('discord.js');
+        const person = message.mentions.users.first();
+
         if(message.mentions.members.size >= 1) {
             if(person.id == message.author.id) {
                 message.channel.send('Eh? Why did you boop yourself?')
@@ -16,13 +83,14 @@ module.exports = {
                 const embed = new Discord.MessageEmbed()
         
                 .setAuthor(`${message.author.username} booped ${person.username}'s nose !`, message.author.displayAvatarURL({ dynamic: true }))
-                .setImage(img)
-                .setURL(boop)
+                .setImage(httpGetAsync(search_url,tenorCallback_search))
                 .setColor(0x4AEFBA)
                 message.channel.send(embed);
             }
         }else {
             message.channel.send('You have to ping someone to boop them')
         }
+
+        
     }
 }
