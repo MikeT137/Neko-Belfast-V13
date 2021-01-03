5e6b49d4cd4c601bef96f8272dc878c9b511ba4d
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-01-03 17:54:04 +0200

    Update blush.js

--- commands/self roleplay/blush.js
+++ commands/self roleplay/blush.js
@@ -4,29 +4,22 @@ module.exports = {
     run: async (bot, message, args) => {
         const Discord = require('discord.js');
         const request = require('request');
+        const fetch = require('node-fetch');
 
-        var gifs = {
-            url: 'https://api.otakugifs.xyz/gif/blush',
-            method: 'GET',
-            headers: {
-                'X-API-KEY': 'pj7g9seujJByserio0awmvx66W8fFtrboW9kVVNeu13yHbBgE3IsgNBS3rUuD8321l2CH3tST900dhEyd0qH9P2',
-            }
-        }
-        const key = 'pj7g9seujJByserio0awmvx66W8fFtrboW9kVVNeu13yHbBgE3IsgNBS3rUuD8321l2CH3tST900dhEyd0qH9P2';
-        var URL = "https://api.otakugifs.xyz/gif/blush" + "&key=" + key;
-
-
-        request(gifs, function(URL, key) {
-            if (!args[0]){
-                const embed = new Discord.MessageEmbed()
+    fetch('https://api.otakugifs.xyz/gif/blush', {
+        method: 'GET',
+        headers: {
+            'X-API-KEY': 'pj7g9seujJByserio0awmvx66W8fFtrboW9kVVNeu13yHbBgE3IsgNBS3rUuD8321l2CH3tST900dhEyd0qH9P2',
+        },
+    }).then(res=> console.log(res));
+        if (!args[0]){
+            const embed = new Discord.MessageEmbed()
     
-                .setAuthor(`${message.author.username} is blushing`, message.author.displayAvatarURL({ dynamic: true }))
-                .setImage(URL[Math.floor(Math.random() * URL.length)])
-                .setFooter('powered by otakugifs.xyz')
-                .setColor(0x4AEFBA)
-                message.channel.send(embed)
-                
-            }
-        })
+            .setAuthor(`${message.author.username} is blushing`, message.author.displayAvatarURL({ dynamic: true }))
+            .setImage(URL[Math.floor(Math.random() * URL.length)])
+            .setFooter('powered by otakugifs.xyz')
+            .setColor(0x4AEFBA)
+            message.channel.send(embed)
+        }
     }
 }
