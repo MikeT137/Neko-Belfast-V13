829171aa9713f89811b55b1a7b69107537b074ae
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-01-04 18:02:42 +0200

    Update blush.js

--- commands/self roleplay/blush.js
+++ commands/self roleplay/blush.js
@@ -19,7 +19,7 @@ module.exports = {
                 const embed = new Discord.MessageEmbed()
         
                 .setAuthor(`${message.author.username} is blushing`, message.author.displayAvatarURL({ dynamic: true }))
-                .setImage(res.url[Math.floor(Math.random() * res.url.length)])
+                .setImage(url[Math.floor(Math.random() * url.length)])
                 .setFooter('powered by otakugifs.xyz')
                 .setColor(0x4AEFBA)
                 message.channel.send(embed)
