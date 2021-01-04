3f015c735c9e2157583d8b06847a395fdfd4167d
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-01-04 17:48:00 +0200

    Update blush.js

--- commands/self roleplay/blush.js
+++ commands/self roleplay/blush.js
@@ -4,10 +4,10 @@ module.exports = {
     run: async (bot, message, args) => {
         const Discord = require('discord.js');
         const fetch = require('node-fetch');
+        url: 'https://api.otakugifs.xyz/gif/blush'
 
         fetch(url, {
             method: 'GET',
-            url: 'https://api.otakugifs.xyz/gif/blush',
             headers: {
                 'X-API-KEY': 'pj7g9seujJByserio0awmvx66W8fFtrboW9kVVNeu13yHbBgE3IsgNBS3rUuD8321l2CH3tST900dhEyd0qH9P2',
             },
