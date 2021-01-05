1bbc2833798f667a0aaabf38f641d87315dc2a2b
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-01-05 19:38:41 +0200

    fg

--- commands/self roleplay/bored.js
+++ commands/self roleplay/bored.js
@@ -3,26 +3,25 @@ module.exports = {
     description: "The user is bored",
     run: async (bot, message, args) => {
         const Discord = require('discord.js');
-        const fetch = require('node-fetch');
-        const url = 'https://api.otakugifs.xyz/gif/bored';
+        const random_bored = [
+            'https://i.pinimg.com/originals/40/ca/73/40ca73fab872f7a10f66f3dcafd09585.gif',
+            'https://media1.tenor.com/images/4b84b1b5b40033e0a3a0bb2e22b7230d/tenor.gif?itemid=16728079',
+            'https://i.pinimg.com/originals/c3/a1/9c/c3a19cf3e79aab4ac4ea674e9764a5ab.gif',
+            'https://i.gifer.com/JHyT.gif',
+            'https://pa1.narvii.com/6061/fdc1d2f595cdcaa769177ec4151e9778847036c0_hq.gif',
+            'https://media1.tenor.com/images/0f0805c286205da154cce414aa110b98/tenor.gif?itemid=8118376',
+            'https://i.pinimg.com/originals/cd/2c/c6/cd2cc65ff07f62533821f952e3542bd8.gif',
+            'https://i.gifer.com/JPTs.gif',
+            'https://i.imgur.com/EzAWv47.gif',
+            'https://2.bp.blogspot.com/-RsPepQekbus/UsKt6g1gMDI/AAAAAAAACEY/78y2DEakcDo/s640/Bored+004.gif'
+        ]
+        if (!args[0]){
+            const embed = new Discord.MessageEmbed()
 
-        fetch(url, {
-            method: 'GET',
-            headers: {
-                'X-API-KEY': 'pj7g9seujJByserio0awmvx66W8fFtrboW9kVVNeu13yHbBgE3IsgNBS3rUuD8321l2CH3tST900dhEyd0qH9P2',
-            },
-        }).then((res) =>
-            res.json()
-        ).then((json) => {
-            if (!args[0]) {
-                const embed = new Discord.MessageEmbed()
-        
-                .setAuthor(`${message.author.username} is bored`, message.author.displayAvatarURL({ dynamic: true }))
-                .setImage(json.url)
-                .setFooter('Powered by otakugifs.xyz')
-                .setColor(0x4AEFBA)
-                message.channel.send(embed)
-            }
-        })
+            .setAuthor(`${message.author.username} is bored`, message.author.displayAvatarURL({ dynamic: true }))
+            .setImage(random_bored[Math.floor(Math.random() * random_bored.length)])
+            .setColor(0x4AEFBA)
+            message.channel.send(embed);
+        }
     }
 }
