24ae28efd09eacca122f82dd9f06e609703d108c
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-01-07 10:40:03 +0200

    Update storyrun.js

--- commands/fun/storyrun.js
+++ commands/fun/storyrun.js
@@ -14,6 +14,27 @@ module.exports = {
             const filter = (reaction, user) => {
                 return ['👍', '✊', '👎'].includes(reaction.emoji.name) && user.id === message.author.id;
             }
+            const filter2 = (reaction2, user) => {
+                return ['👍', '✊', '👎'].includes(reaction2.emoji.name) && user.id === message.author.id;
+            };
+            const filter3 = (reaction3, user) => {
+                return ['👍', '✊', '👎'].includes(reaction3.emoji.name) && user.id === message.author.id;
+            };
+            const filter4 = (reaction4, user) => {
+                return ['👍', '✊', '👎'].includes(reaction4.emoji.name) && user.id === message.author.id;
+            };
+            const filter5 = (reaction5, user) => {
+                return ['👍', '✊', '👎'].includes(reaction5.emoji.name) && user.id === message.author.id;
+            };
+            const filter6 = (reaction6, user) => {
+                return ['👍', '✊', '👎'].includes(reaction6.emoji.name) && user.id === message.author.id;
+            };
+            const filter7 = (reaction7, user) => {
+                return ['👍', '✊', '👎'].includes(reaction7.emoji.name) && user.id === message.author.id;
+            };
+            const filter8 = (reaction8, user) => {
+                return ['👍', '✊', '👎'].includes(reaction8.emoji.name) && user.id === message.author.id;
+            };
 
             msg.awaitReactions(filter, { max: 1 })
                 .then(collected => {
@@ -21,7 +42,7 @@ module.exports = {
                     if(reaction.emoji.name === '👍') {
                         msg.edit('*You think to yourself that it was just an animal or something, so you decide to ignore it. You then finish chopping the wood, you grab all of it, and go towards your basement so you can leave it there. But then you hear that weird sound again, this time coming from your basement. You then:\n\n👍 - go in the basement\n👎 - leave the cabin*\n\n**Select your choice:**')
 
-                        msg.awaitReactions(filter, { max: 1 })
+                        msg.awaitReactions(filter2, { max: 1 })
                         .then(collected2 => {
                             const reaction2 = collected2.first();
                             if(reaction2.emoji.name === '👍') {
@@ -34,13 +55,13 @@ module.exports = {
                             if(reaction2.emoji.name === '👎') {
                                 msg.edit('*You were too scared to go in the basement so you left the cabin because who knows what could be down there, it might come to the surface! So you decide to leave that place and walk in the forest. Surprisingly, the forest was very quiet, no animals, no birds, nothing. But suddenly, you hear some siren noises that aren\'t too far from where you are. You start hiding. And then a voice the that sounds like being on a radio says:* Can anyone hear me ? I\'m looking for a 20 year old boy, is anyone there ? *Hearing that, you then:\n\n👍 - run away\n👎 - get closer and say hi*\n\n**Select your choice:**')
 
-                                msg.awaitReactions(filter, { max: 1 })
+                                msg.awaitReactions(filter3, { max: 1 })
                                 .then(collected3 => {
                                     const reaction3 = collected3.first();
                                     if(reaction3.emoji.name === '👍') {
                                         msg.edit('*You thought that something was a little fishy so you instead run away from the voice. But as you run, you then see a weird and tall creature with a siren for its head walking through the forest, the creature sees you and it starts to run straight at you doing some siren noises. You immediately get scared and you run for your life as fast as you could, until suddenly you see another cabin. And you go inside the cabin and close the door behind you without knowing who\'s cabin it was. And the you see a girl that starts to point a gun at you.*\n\ngirl: Who are you and why did you come in my house?\nyou: I was chased by that siren headed thing. Sorry for intruding in your house\ngirl: Its alright, you can stay, but dont touch me okay?\nyou: I wasn\'t going to but okay\n\n*Suddenly you hear something bashing on the door really hard like they were trying to break it. Than the girl says:* We need to get you covered. Here, take this gun. *In that moment you:\n\n👍 - try both of you to kill the creature by shooting at it\n👎 - exit the house through the backdoor and run away*\n\n**Select your choice:**')
                                         
-                                        msg.awaitReactions(filter, { max: 1 })
+                                        msg.awaitReactions(filter4, { max: 1 })
                                         .then(collected4 => {
                                             const reaction4 = collected4.first();
                                             if(reaction4.emoji.name === '👍') {
@@ -53,13 +74,13 @@ module.exports = {
                                             if(reaction4.emoji.name === '👎') {
                                                 msg.edit('*You and the girl got through the back door and ran away in the woods. Fortunately, the creature didn\'t see in what direction you went, so you run and run and run, until eventually you take a break. The night came. You make a fire so you can warm yourselves. But the night in the forest is never to be trusted. So both you and the girl stay awake in case the creature finds you. At last, you fall asleep, but the girl stays awake. She then hears a branch breaking.* Who\'s there ? *she said. But of course there was no response. And then the girl hears weird noises like static as she could see the creature from afar.*\n\ngirl: You gotta get up RIGHT NOW\nyou: Why? let me sleep a little more..\ngirl: Because the creature is coming to get us, idiot!\n\n*She then grabs you and you both start running more from the creature. An hour later, you escape from the creature. But you then started to get hungry. So you both kill a deer and eat it.*\n\ngirl: We have to do something about this, we cant just keep running forever\nyou:\n\n👍 - We could try to kill it from a distance.\n👎 - Let\'s try to \"tank it\", we have 2 guns and 1 axe, can\'t be that hard.\n\n**Select your choice:**')
 
-                                                msg.awaitReactions(filter, { max: 1 })
+                                                msg.awaitReactions(filter5, { max: 1 })
                                                 .then(collected5 => {
                                                     const reaction5 = collected5.first();
                                                     if(reaction5.emoji.name === '👍') {
                                                         msg.edit('*You and the girl separate each other, as she watches you getting ready with her gun. And then you loudly say:* Siren head, i\'m here! *You then hear the siren noises, but it was coming from a little far then where you\'re at. And then you hear a girl\'s scream as she craves for help while being killed. You then run again, but eventually you find a lake, with a very deep fog that you can barely see through it. You go in the fog, and you throw a rock very far from you to your left. The creature goes to that sound, meanwhile you walk quietly through the fog until eventually you see a watch tower. You decided to go inside the watch tower. Once you got up there you see a sniper and a radio. You:\n\n👍 - you get the sniper and destroy that siren head\n👎 - get the radio and call for help*\n\n**Select your choice:**')
 
-                                                        msg.awaitReactions(filter, { max: 1 })
+                                                        msg.awaitReactions(filter6, { max: 1 })
                                                         .then(collected6 => {
                                                             const reaction6 = collected6.first();
                                                             if(reaction6.emoji.name === '👍') {
@@ -103,7 +124,7 @@ module.exports = {
                     if(reaction.emoji.name === '✊') {
                         msg.edit('*The noises started to get louder and louder. And so because you were scared you tried to call for help, but there was no signal. And then a tall creature with a siren for a head gets closer to your cabin. You:*\n\n👍 - go on bed and hide yourself with a blanket\n👎 - go outside and try to run away from the siren head*\n\n**Select your choice:**')
 
-                        msg.awaitReactions(filter, { max: 1 })
+                        msg.awaitReactions(filter7, { max: 1 })
                         .then(collected7 => {
                             const reaction7 = collected7.first();
                             if(reaction7.emoji.name === '👍') {
@@ -125,7 +146,7 @@ module.exports = {
                     if(reaction.emoji.name === '👎') {
                         msg.edit('*You then decide to investigate the zone. But then you see a tall creature with a siren for a head. The creature sees you and runs at you really fast, and then your reflexes starts to kick in and you hit his leg with your axe and then you quickly run away. And because you were very close to your cabin, you thought it wasn\'t the best idea to hide in your cabin. You then start running away from him and eventually you see a hunter with a double barrel shotgun driving near you. You then say:* Help! This creature is chasing me! *And then you get in the car and the hunter drives away from the monster. It was a long ride, but eventually you arrive at the hunter\'s cabin. You then sit down at the table with him and have a chat.*\n\nhunter: What was that creature?\nyou: I don\'t know, but it definitely wasn\'t friendly. I was just doing my thing, cutting wood and all, and then i heard some siren noises coming from the forest. Though it was my mistake that i went to check what it was. Thanks for saving me from that thing\nhunter: You\'re welcome. Come on, eat up, i imagine you\'re pretty hungry.\nyou: Ah, thanks.\n\n*You then start eating, but whatever it was, it had a weird flavour. After you\'re done eating. You then go to sleep. The hunter said he\'ll go to sleep too. But he lied. And he slowly walks in with a knife and a fork towards you. You then realise that the hunter was a cannibal. You then:\n\n👍 - try to fight him\n👎 - get your axe and throw it at him*\n\n**Select your choice:**')
 
-                        msg.awaitReactions(filter, { max: 1 })
+                        msg.awaitReactions(filter8, { max: 1 })
                         .then(collected8 => {
                             const reaction8 = collected8.first();
                             if(reaction8.emoji.name === '👍') {
