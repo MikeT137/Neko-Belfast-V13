2b9788e3c7f55fd49d7011beeb28c9ba1a5b90f6
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-01-09 08:42:51 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -4,20 +4,20 @@ module.exports = {
     run: async (bot, message, args) => {
         let amount;
 
-        if(message.deletable) {
-            message.delete();
-        }else if(!message.member.hasPermission("ADMINISTRATOR")) {
+        if(!message.member.hasPermission("ADMINISTRATOR")) {
             message.channel.send('You cant delete messages')
         }else if(isNaN(args[0]) || parseInt(args[0]) <= 0) {
             message.channel.send('You didn\'t tell me how many messages to delete')
-        }else {
+        }else if(message.deletable) {
+
             if(parseInt(args[0]) > 100) {
                 amount = 100;
             }else {
                 amount = parseInt(args[0]);
             }
+
             message.channel.bulkDelete(amount, true)
             .then(deleted => message.channel.send(`I deleted \`${deleted.size}\` messages`))
-            }
+        }else return;
     }
 }
