c1b903fee81268c416586285edad1c0cd2ad99bf
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-01-09 08:37:46 +0200

    Update clear.js

--- commands/moderation/clear.js
+++ commands/moderation/clear.js
@@ -6,20 +6,18 @@ module.exports = {
 
         if(message.deletable) {
             message.delete();
-        }
-        if(!message.member.hasPermission("ADMINISTRATOR")) {
+        }else if(!message.member.hasPermission("ADMINISTRATOR")) {
             message.channel.send('You cant delete messages')
-        }
-        if(isNaN(args[0]) || parseInt(args[0]) <= 0) {
+        }else if(isNaN(args[0]) || parseInt(args[0]) <= 0) {
             message.channel.send('You didn\'t tell me how many messages to delete')
-        }
-        
-        if(parseInt(args[0]) > 100) {
-            amount = 100;
         }else {
-            amount = parseInt(args[0]);
-        }
-        message.channel.bulkDelete(amount, true)
-        .then(deleted => message.channel.send(`I deleted \`${deleted.size}\` messages`))
+            if(parseInt(args[0]) > 100) {
+                amount = 100;
+            }else {
+                amount = parseInt(args[0]);
+            }
+            message.channel.bulkDelete(amount, true)
+            .then(deleted => message.channel.send(`I deleted \`${deleted.size}\` messages`))
+            }
     }
 }
