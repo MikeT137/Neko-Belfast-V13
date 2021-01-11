ac171c3d444222f9d08b1c4517c41f94a605042b
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-01-11 22:09:39 +0200

    Update meme.js

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -4,16 +4,20 @@ module.exports = {
     name:'meme',
     description: "Gives the user a meme",
     run: async (bot, message, args) => {
+        const random = subreddits[Math.floor(Math.random() * subreddits.length)];
+        const img = await randomPuppy(random);
+        const img2 = await randomPuppy(subreddit);
+
         if(!args[0]) {
             var subreddits = [
+                "Animemes",
                 "dankmemes",
-                "memes",
                 "goodanimemes",
+                "goodanimememes",
+                "memes",
                 "wholesomememes",
-                "wholesomeanimemes",
+                "wholesomeanimemes"
             ]
-            const random = subreddits[Math.floor(Math.random() * subreddits.length)];
-            const img = await randomPuppy(random);
 
             var embed = new Discord.MessageEmbed()
 
@@ -24,38 +28,32 @@ module.exports = {
             message.channel.send(embed);
 
         }else if(args[0] == 'dank') {
-            const subreddits = [
-                "dankmemes",
-            ]
-            const random = subreddits[Math.floor(Math.random() * subreddits.length)];
-            const img = await randomPuppy(random);
+            const subreddit = "dankmemes"
 
             const embed = new Discord.MessageEmbed()
 
-            .setTitle(`From r/${random}`)
-            .setImage(img)
-            .setURL(`https://reddit.com/r/${random}/new`)
+            .setTitle(`From r/${subreddit}`)
+            .setImage(img2)
+            .setURL(`https://reddit.com/r/${subreddit}/new`)
             .setColor(0x4AEFBA)
             message.channel.send(embed);
 
-        }else if(args[0] == 'memes') {
-            const subreddits = [
-                "memes",
-            ]
-            const random = subreddits[Math.floor(Math.random() * subreddits.length)];
-            const img = await randomPuppy(random);
+        }else if(args[0] == 'memes' || args[0] == 'meme') {
+            const subreddit = "memes"
 
             const embed = new Discord.MessageEmbed()
 
-            .setTitle(`From r/${random}`)
-            .setImage(img)
-            .setURL(`https://reddit.com/r/${random}/new`)
+            .setTitle(`From r/${subreddit}`)
+            .setImage(img2)
+            .setURL(`https://reddit.com/r/${subreddit}/new`)
             .setColor(0x4AEFBA)
             message.channel.send(embed);
 
         }else if(args[0] == 'anime') {
             const subreddits = [
+                "Animemes",
                 "goodanimemes",
+                "goodanimememes"
             ]
             const random = subreddits[Math.floor(Math.random() * subreddits.length)];
             const img = await randomPuppy(random);
@@ -69,32 +67,24 @@ module.exports = {
             message.channel.send(embed);
 
         }else if(args[0] == 'wholesome') {
-            const subreddits = [
-                "wholesomememes",
-            ]
-            const random = subreddits[Math.floor(Math.random() * subreddits.length)];
-            const img = await randomPuppy(random);
+            const subreddit = "wholesomememes"
 
             const embed = new Discord.MessageEmbed()
 
-            .setTitle(`From r/${random}`)
-            .setImage(img)
-            .setURL(`https://reddit.com/r/${random}/new`)
+            .setTitle(`From r/${subreddit}`)
+            .setImage(img2)
+            .setURL(`https://reddit.com/r/${subreddit}/new`)
             .setColor(0x4AEFBA)
             message.channel.send(embed);
 
         }else if(args[0] == 'wholesomeanime' || args[0] == 'wanime') {
-            const subreddits = [
-                "wholesomeanimemes",
-            ]
-            const random = subreddits[Math.floor(Math.random() * subreddits.length)];
-            const img = await randomPuppy(random);
+            const subreddit = "wholesomeanimemes"
 
             const embed = new Discord.MessageEmbed()
 
-            .setTitle(`From r/${random}`)
-            .setImage(img)
-            .setURL(`https://reddit.com/r/${random}/new`)
+            .setTitle(`From r/${subreddit}`)
+            .setImage(img2)
+            .setURL(`https://reddit.com/r/${subreddit}/new`)
             .setColor(0x4AEFBA)
             message.channel.send(embed);
         }
