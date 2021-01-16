7fd94da54cde3667f27e07d2ebba132e7a5f9290
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-01-16 14:42:51 +0200

    Update suggest.js

--- commands/moderation/suggest.js
+++ commands/moderation/suggest.js
@@ -7,10 +7,10 @@ module.exports = {
         if(!args[0]) {
             message.channel.send('You have to say something to suggest it to the creator')
         }else if(args[0]) {
-            bot.users.fetch(`${mikuid}`).then((user) =>{
+            bot.users.fetch(`${mikuid}`).then((user) => {
                 user.send(`${message.author} said: ${args.join(' ')}`);
                 message.channel.send('Your suggestion has been sent to the creator, thank you for your time<3')
-              });
+            });
         }else return;
     }
 }
