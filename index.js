e95c35233549ac158683a7892e82571f3a75f6e3
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-01-16 19:28:07 +0200

    Remove lewd.js

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ const topgglink = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcyNzA5MzIzNjk1
 const dbl = new DBL(`${topgglink}`, bot);
 
 dbl.on('posted', () => {
-console.log('Server count posted!');
+    console.log('Server count posted!');
 })
 
 dbl.on('error', e => {
-console.log(`Oops! ${e}`);
+    console.log(`Oops! ${e}`);
 });
 
 //Token
