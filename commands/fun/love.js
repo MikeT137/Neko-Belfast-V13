ca58227c8983d90da1c09dbae98beddae1401353
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-01-21 17:25:02 +0200

    Update love.js

--- commands/fun/love.js
+++ commands/fun/love.js
@@ -23,38 +23,5 @@ module.exports = {
                 message.channel.send(embed);
             }
         }
-        /*const fetch = require('node-fetch');
-        const url = 'https://api.otakugifs.xyz/gif/cuddle';
-
-        fetch(url, {
-            method: 'GET',
-            headers: {
-                'X-API-KEY': 'pj7g9seujJByserio0awmvx66W8fFtrboW9kVVNeu13yHbBgE3IsgNBS3rUuD8321l2CH3tST900dhEyd0qH9P2',
-            },
-        }).then((res) =>
-            res.json()
-        ).then((json) => {
-            if (!args[0]){
-                const embed = new Discord.MessageEmbed()
-    
-                    .setAuthor(`Nyaa~, i love you ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
-                    .setImage(json.url)
-                    .setFooter('Powered by otakugifs.xyz')
-                    .setColor('#7d77df')
-                message.channel.send(embed);
-            }else if(args[0] || message.mentions.members.size >= 1) {
-                if(person.id == message.author.id) {
-                    message.channel.send('Yay! Always give appreciation and love to yourself nya~')
-                }else {
-                    const embed = new Discord.MessageEmbed()
-            
-                    .setAuthor(`${message.author.username} loves ${person.username}!`, message.author.displayAvatarURL({ dynamic: true }))
-                    .setImage(json.url)
-                    .setFooter('Powered by otakugifs.xyz')
-                    .setColor('#7d77df')
-                    message.channel.send(embed);
-                }
-            }else return;
-        })*/
     }
 }
