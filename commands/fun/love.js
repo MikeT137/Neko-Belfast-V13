63319147fc273d1f4aa50998ab16d996abf66c70
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-01-21 18:17:35 +0200

    Update love.js

--- commands/fun/love.js
+++ commands/fun/love.js
@@ -4,7 +4,7 @@ module.exports = {
     run: async (bot, message, args) => {
         const Discord = require('discord.js');
         const person = message.mentions.users.first();
-        let person2 = message.guild.members.filter(m => m.id !== message.author.id).random().user;
+        let person2 = message.guild.members.cache.filter(m => m.id !== message.author.id).random().user;
         const love = Math.random() * 100;
         const loveIndex = Math.floor(love / 10);
         const loveLevel = "💖".repeat(loveIndex) + "💔".repeat(10 - loveIndex);
