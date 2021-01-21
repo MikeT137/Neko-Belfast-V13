7c2a0f4382ac3654b35999199d16dcc847e88e68
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-01-21 17:59:14 +0200

    Update love.js

--- commands/fun/love.js
+++ commands/fun/love.js
@@ -4,12 +4,19 @@ module.exports = {
     run: async (bot, message, args) => {
         const Discord = require('discord.js');
         const person = message.mentions.users.first();
+        const person2 = message.guild.members.filter(m => m.id !== message.author.id).random();
         const love = Math.random() * 100;
         const loveIndex = Math.floor(love / 10);
         const loveLevel = "💖".repeat(loveIndex) + "💔".repeat(10 - loveIndex);
 
         if(!args[0]) {
-            message.channel.send('You must ping someone to see the love-affinity level')
+            const embed = new Discord.MessageEmbed()
+
+                .setAuthor(`${message.author.username} and ${person2.username} love eachother this much:`, message.author.displayAvatarURL({ dynamic: true }))
+                .setThumbnail(person2.displayAvatarURL({dynamic: true}))
+                .addField(`💟 ${Math.floor(love)}%`, `${loveLevel}`)
+                .setColor('#7d77df')
+            message.channel.send(embed);
         }else if(message.mentions.members.size == 1) {
             if(person.id == message.author.id) {
                 message.channel.send('You can\'t do this command to yourself');
