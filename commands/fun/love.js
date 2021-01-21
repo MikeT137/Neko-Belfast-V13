533fc6e87b863011d58d73a309717aa2e6dfd5ad
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-01-21 18:40:54 +0200

    Update love.js

--- commands/fun/love.js
+++ commands/fun/love.js
@@ -4,19 +4,20 @@ module.exports = {
     run: async (bot, message, args) => {
         const Discord = require('discord.js');
         const person = message.mentions.users.first();
-        let person2 = message.guild.members.random();
+        //let person2 = message.guild.membersfilter(m => m.id !== message.author.id).random().user;
         const love = Math.random() * 100;
         const loveIndex = Math.floor(love / 10);
         const loveLevel = "💖".repeat(loveIndex) + "💔".repeat(10 - loveIndex);
 
         if(!args[0]) {
-            const embed = new Discord.MessageEmbed()
+            /*const embed = new Discord.MessageEmbed()
 
                 .setAuthor(`${message.author.username} and ${person2.username} love eachother this much:`, message.author.displayAvatarURL({ dynamic: true }))
                 .setThumbnail(person2.displayAvatarURL({dynamic: true}))
                 .addField(`💟 ${Math.floor(love)}%`, `${loveLevel}`)
                 .setColor('#7d77df')
-            message.channel.send(embed);
+            message.channel.send(embed);*/
+            message.channel.send('You have to ping someone for the command to work')
         }else if(message.mentions.members.size == 1) {
             if(person.id == message.author.id) {
                 message.channel.send('You can\'t do this command to yourself');
