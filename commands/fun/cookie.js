64c14e2ceeb2c39c468436d8373da5862dcccaf0
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-01-30 09:52:34 +0200

    Create cookie.js

---
+++ commands/fun/cookie.js
@@ -0,0 +1,48 @@
+module.exports = {
+    name:'cookie',
+    description: "The user can get or give cookies",
+    run: async (bot, message, args) => {
+        const Discord = require('discord.js');
+        const person = message.mentions.users.first();
+
+        const fetch = require('node-fetch');
+        const url = 'https://api.otakugifs.xyz/gif/cookie';
+
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'X-API-KEY': 'pj7g9seujJByserio0awmvx66W8fFtrboW9kVVNeu13yHbBgE3IsgNBS3rUuD8321l2CH3tST900dhEyd0qH9P2',
+            },
+        }).then((res) =>
+            res.json()
+        ).then((json) => {
+            if(message.mentions.members.size >= 1) {
+                if(person.id == message.author.id) {
+                    const embed = new Discord.MessageEmbed()
+        
+                        .setAuthor(`${message.author.username} just got a cookie :cookie:`, message.author.displayAvatarURL({ dynamic: true }))
+                        .setImage(json.url)
+                        .setFooter('Powered by otakugifs.xyz')
+                        .setColor('#7d77df')
+                    message.channel.send(embed)
+                }else {
+                    const embed = new Discord.MessageEmbed()
+        
+                        .setAuthor(`${message.author.username} gave a cookie to ${person.username}, how nice :cookie:`, message.author.displayAvatarURL({ dynamic: true }))
+                        .setImage(json.url)
+                        .setFooter('Powered by otakugifs.xyz')
+                        .setColor('#7d77df')
+                    message.channel.send(embed)
+                }
+            }else if (!args[0]){
+                const embed = new Discord.MessageEmbed()
+        
+                    .setAuthor(`${message.author.username} just got a cookie :cookie:`, message.author.displayAvatarURL({ dynamic: true }))
+                    .setImage(json.url)
+                    .setFooter('Powered by otakugifs.xyz')
+                    .setColor('#7d77df')
+                message.channel.send(embed)
+            }else return;
+        })
+    }
+}
