48683d4b33ff7998a0b8653b63c5e98ee2af5db5
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-02-01 09:44:34 +0200

    Update enlarge.js

--- commands/moderation/enlarge.js
+++ commands/moderation/enlarge.js
@@ -2,7 +2,7 @@ module.exports = {
     name:'enlarge',
     description: "It sends a bigger pic of the emoji sent",
     run: async (bot, message, args) => {
-        const emote = bot.emojis.cache.get(emoji.id);
+        const emote = bot.emojis.cache.get(args[0].id);
 
         if(!args[0]) {
             message.channel.send('You need to tell me which emoji you want me to enlarge')
