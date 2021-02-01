bbb9ea856053c59ada748b29d89534f10c44442f
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-02-01 09:14:06 +0200

    Create enlarge.js

---
+++ commands/moderation/enlarge.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name:'enlarge',
+    description: "It sends a bigger pic of the emoji sent",
+    run: async (bot, message, args) => {
+        const emoji = bot.emojis.get(emojiID);
+
+        if(!args[0]) {
+            message.channel.send('You need to tell me which emoji you want me to enlarge')
+        }else {
+            await message.channel.send({files: [
+            {
+                attachment: emoji.url,
+                name: emoji.name + '.png'
+            }
+            ]});
+        }
+    }
+}
