ed3782281e8ec1400dd68236f74531439342b09b
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-02-03 09:10:15 +0200

    Update botinfo.js

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -20,7 +20,7 @@ module.exports = {
                     `\`Developer:\` ${author} :flag_ro:`,
                     `\`Profile picture artist:\` [u/Linkool21](https://www.reddit.com/user/Linkool21/)`
                 ])
-                .addField('Want to support me? You can do so by:', '[Inviting me!](https://discord.com/oauth2/authorize?client_id=727093236954431488&scope=bot&permissions=2146958847) [Joining my server!](https://discord.gg/xSkkeVf) [Voting me!](https://top.gg/bot/727093236954431488/vote) [Becoming my patreon!](https://www.patreon.com/user?u=10619598)')
+                .addField('Want to support me? You can do so by:', '[\`Inviting me!\`](https://discord.com/oauth2/authorize?client_id=727093236954431488&scope=bot&permissions=2146958847) [\`Joining my server!\`](https://discord.gg/xSkkeVf) [\`Voting me!\`](https://top.gg/bot/727093236954431488/vote) [\`Becoming my patreon!\`](https://www.patreon.com/user?u=10619598)')
                 .addField('Special Message', '*Hello, Miku here! Thank you for choosing to use this bot, and for being so supportive with me, i know that it takes a while to fix bugs, and it may not be the best bot out there but still, thank you for using my bot, it means everything. Anyway, i hope you\'re enjoying the features of my bot so far, and in the meantime, stay safe and have a great day :heart:*')
                 .setColor('#7d77df')
             message.channel.send(embed);
