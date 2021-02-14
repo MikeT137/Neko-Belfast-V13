6e494e3067e72d7615aee56df88d0e64ab72dc53
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-02-15 00:42:48 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -11,55 +11,54 @@ module.exports = bot => {
     const newyear = schedule.scheduleJob({start: startTime, end: endTime, rule: '* * 1 1 *'}, function() {
         bot.user.setActivity(`Happy New Year nya~! | ${prefix}help`, {type: 'PLAYING'}).catch(console.error);
     })
-    newyear.cancel()
+    newyear.cancel();
 
     const valentine = schedule.scheduleJob({start: startTime, end: endTime, rule: '* * 14 2 *'}, function() {
         bot.user.setActivity(`Will you be my Valentine? | ${prefix}help`, {type: 'PLAYING'}).catch(console.error);
     })
-    valentine.cancel()
+    valentine.cancel();
 
     const easter = schedule.scheduleJob({start: startTime, end: endTime, rule: '* * 8 4 *'}, function() {
         bot.user.setActivity(`Happy Easter nya~! | ${prefix}help`, {type: 'PLAYING'}).catch(console.error);
     })
-    easter.cancel()
+    easter.cancel();
 
     const bel = schedule.scheduleJob({start: startTime, end: endTime, rule: '* * 29 6 *'}, function() {
         bot.user.setActivity(`It\'s my birthday nya~! | ${prefix}help`, {type: 'PLAYING'}).catch(console.error);
     })
-    bel.cancel()
+    bel.cancel();
 
     const miku = schedule.scheduleJob({start: startTime, end: endTime, rule: '* * 16 9 *'}, function() {
         bot.user.setActivity(`Happy birthday Miku-senpai! | ${prefix}help`, {type: 'PLAYING'}).catch(console.error);
     })
-    miku.cancel()
+    miku.cancel();
 
     const halloween = schedule.scheduleJob({start: startTime, end: endTime, rule: '* * 31 10 *'}, function() {
         bot.user.setActivity(`Have a spooky Halloween teehee | ${prefix}help`, {type: 'PLAYING'}).catch(console.error);
     })
-    halloween.cancel()
+    halloween.cancel();
 
     const thanks = schedule.scheduleJob({start: startTime, end: endTime, rule: '* * 25 11 *'}, function() {
         bot.user.setActivity(`Happy Thanksgiving nya~! | ${prefix}help`, {type: 'PLAYING'}).catch(console.error);
     })
-    thanks.cancel()
+    thanks.cancel();
 
     const xmas = schedule.scheduleJob({start: startTime, end: endTime, rule: '* * 25 12 *'}, function() {
         bot.user.setActivity(`Merry Xmas nya~! | ${prefix}help`, {type: 'PLAYING'}).catch(console.error);
     })
-    xmas.cancel()
+    xmas.cancel();
 
     /*
     Holidays
 
     January 1st - Happy New Year nya~!
-    February 14th - Will you be my Valentine nya~?
-    March 17th - Happy St. Patrick\'s Day nya~!
+    February 14th - Will you be my Valentine?
     April 8th - Happy Easter nya~!
     June 29th - It\'s my birthday nya~!
     September 16th - Happy birthday Miku-senpai!
-    October 31st - Have a spooky Halloween! Tee hee
+    October 31st - Have a spooky Halloween tee hee
     November 25th - Happy Thanksgiving nya~!
-    December 25th - Merry Christmas nya!
+    December 25th - Merry Xmas nya!
 
     Every other day - with my tail nya~
     */
