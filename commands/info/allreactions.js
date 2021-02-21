b9cf685db41dda3f97ead3f9993a9182537e0d98
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-02-21 10:03:29 +0200

    Create allreactions.js

---
+++ commands/info/allreactions.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name:'allreactions',
+    description: "The author checks for new reactions on the otakugifs.xyz site",
+    run: async (bot, message, args) => {
+        const Discord = require('discord.js');
+        const fetch = require('node-fetch');
+        const url = 'https://api.otakugifs.xyz/allreactions';
+
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'X-API-KEY': 'pj7g9seujJByserio0awmvx66W8fFtrboW9kVVNeu13yHbBgE3IsgNBS3rUuD8321l2CH3tST900dhEyd0qH9P2',
+            },
+        }).then((res) =>
+            res.json()
+        ).then((json) => {
+            if (!args[0]) {
+                message.channel.send(`Reactions: ${json.url}`)
+            }
+        })
+    }
+}
