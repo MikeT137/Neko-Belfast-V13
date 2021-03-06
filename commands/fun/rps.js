6af422b5bb919aeb8affbd4254af0c46c6dd146e
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-03-06 16:44:04 +0200

    Update rps.js

--- commands/fun/rps.js
+++ commands/fun/rps.js
@@ -3,20 +3,19 @@ module.exports = {
     description: 'The user plays rock paper scissors',
     run: async (bot, message, args) => {
         const acceptedReplies = ['rock', 'paper', 'scissors'];
-        const random = [Math.floor(Math.random() * acceptedReplies.length)];
-        const result = acceptedReplies[random];
+        const result = acceptedReplies[Math.floor(Math.random() * acceptedReplies.length)];
 
         if (!args[0]) {
             message.channel.send(`You have to \"choose your weapon\": \`${acceptedReplies.join(', ')}\``);
         }else if (!acceptedReplies.includes(args[0])) {
             message.channel.send(`You can only use: \`${acceptedReplies.join(', ')}\``);
-        }else if (result === args[0]) {
-            message.channel.send(`I played ${result}. It's a tie!`);
         }
         switch (args[0]) {
             case 'rock':
                 if (result === 'paper') {
                     message.channel.send(`I played ${result}. I won!`);
+                }else if (result === args[0]) {
+                    message.channel.send(`I played ${result}. It's a tie!`);
                 }else {
                     message.channel.send(`I played ${result}. You won!`);
                 }
@@ -25,6 +24,8 @@ module.exports = {
             case 'paper':
                 if (result === 'scissors') {
                     message.channel.send(`I played ${result}. I won!`);
+                }else if (result === args[0]) {
+                    message.channel.send(`I played ${result}. It's a tie!`);
                 }else {
                     message.channel.send(`I played ${result}. You won!`);  
                 }      
@@ -33,6 +34,8 @@ module.exports = {
             case 'scissors':
                 if (result === 'rock') {
                     message.channel.send(`I played ${result}. I won!`);
+                }else if (result === args[0]) {
+                    message.channel.send(`I played ${result}. It's a tie!`);
                 }else {
                     message.channel.send(`I played ${result}. You won!`);
                 }
