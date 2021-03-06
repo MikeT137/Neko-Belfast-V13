6e3de35255bf7d6b95490f4cef3551a49a306788
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-03-06 16:23:52 +0200

    Update rps.js

--- commands/fun/rps.js
+++ commands/fun/rps.js
@@ -6,23 +6,37 @@ module.exports = {
         const random = [Math.floor(Math.random() * acceptedReplies.length)];
         const result = acceptedReplies[random];
 
-        if (!args[0]) return message.channel.send(`You have to \"choose your weapon\": \`${acceptedReplies.join(', ')}\``);
-        if (!acceptedReplies.includes(args[0])) return message.channel.send(`You can only use: \`${acceptedReplies.join(', ')}\``);
-        if (result === args[0]) return message.channel.send(`I played ${result}. It's a tie!`);
-        
+        if (!args[0]) {
+            message.channel.send(`You have to \"choose your weapon\": \`${acceptedReplies.join(', ')}\``);
+        }else if (!acceptedReplies.includes(args[0])) {
+            message.channel.send(`You can only use: \`${acceptedReplies.join(', ')}\``);
+        }else if (result === args[0]) {
+            message.channel.send(`I played ${result}. It's a tie!`);
+        }
         switch (args[0]) {
-            case 'rock': {
-                if (result === 'paper') return message.channel.send(`I played ${result}. I won!`);
-                else return message.channel.send(`I played ${result}. You won!`);
-            }
-            case 'paper': {
-                if (result === 'scissors') return message.channel.send(`I played ${result}. I won!`);
-                else return message.channel.send(`I played ${result}. You won!`);        
-            }
-            case 'scissors': {
-                if (result === 'rock') return message.channel.send(`I played ${result}. I won!`);
-                else return message.channel.send(`I played ${result}. You won!`);
-            }
+            case 'rock':
+                if (result === 'paper') {
+                    message.channel.send(`I played ${result}. I won!`);
+                }else {
+                    message.channel.send(`I played ${result}. You won!`);
+                }
+            break;
+            
+            case 'paper':
+                if (result === 'scissors') {
+                    message.channel.send(`I played ${result}. I won!`);
+                }else {
+                    message.channel.send(`I played ${result}. You won!`);  
+                }      
+            break;
+
+            case 'scissors':
+                if (result === 'rock') {
+                    message.channel.send(`I played ${result}. I won!`);
+                }else {
+                    message.channel.send(`I played ${result}. You won!`);
+                }
+            break;
         }
     }
 }
