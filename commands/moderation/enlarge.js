5798f4fb0e11d8ca312dbda40c9539833c692017
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-03-15 20:14:43 +0200

    Update enlarge.js

--- commands/moderation/enlarge.js
+++ commands/moderation/enlarge.js
@@ -2,10 +2,7 @@ module.exports = {
     name:'enlarge',
     description: "It sends a bigger pic of the emoji sent",
     run: async (bot, message, args) => {
-        const Discord = require('discord.js')
-        const emote = new Discord.GuildEmoji
-        this.animated = emote.animated
-        const emoji = bot.emojis.get(emote.id);
+        const emoji = bot.emojis.cache.get(args[0].id);
 
         if(!args[0]) {
             message.channel.send('You need to tell me which emoji you want me to enlarge')
