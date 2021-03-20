1a599fbb7372e0eded20574c3307530c8d27303b
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-03-20 22:26:46 +0200

    Update ship.js

--- commands/fun/ship.js
+++ commands/fun/ship.js
@@ -2,10 +2,10 @@ module.exports = {
     name: 'ship',
     description: 'The user ships two other users',
     run: async (bot, message, args) => {
-        const person = message.mentions.users.first();
-        const person2 = message.mentions.users;
+        const person = message.mentions.users.get(args[0]);
+        const person2 = message.mentions.users.get(args[1]);
 
-        if(message.mentions.members.size == 2 || args[0] == person || args[1] == person2) {
+        if(message.mentions.members.size == 2) {
             message.channel.send(`**${person.username}** :revolving_hearts: **${person2.username} = ${person.username.slice(0, person.username.length / 2)}${person2.username.slice(person2.username.length / 2, -1)}**`)
         }else {
             message.channel.send('You have to ping two users to ship them.')
