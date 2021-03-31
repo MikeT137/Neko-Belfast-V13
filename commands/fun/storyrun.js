b53f2e53b470e54f922095ae51ebd995b5eb3707
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-03-31 09:50:34 +0300

    Update storyrun.js

--- commands/fun/storyrun.js
+++ commands/fun/storyrun.js
@@ -62,14 +62,14 @@ module.exports = {
             const filter14 = (reaction14, user) => {
                 return ['👍', '✊', '👎'].includes(reaction14.emoji.name) && user.id === message.author.id;
             }
-            const userReactions = message.reactions.cache.filter(react => react.users.cache.has(message.author.id));
+            const userReactions = msg.reactions.cache.filter(react => react.users.cache.has(msg.author.id));
 
             msg.awaitReactions(filter, { max: 1 })
                 .then(collected => {
                     const reaction = collected.first();
                     if(reaction.emoji.name === '👍') {
                         for (const react of userReactions.values()) {
-                            react.users.remove(message.author.id)
+                            react.users.remove(msg.author.id)
                         }
                         msg.edit('*You think to yourself that it was just an animal or something, so you decide to ignore it. You then finish chopping the wood, you grab all of it, and go towards your basement so you can leave it there. But then you hear that weird sound again, this time coming from your basement. You then:\n\n👍 - go in the basement\n👎 - leave the cabin*\n\n**Select your choice:**')
 
