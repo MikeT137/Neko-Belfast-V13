dc617760b3810cba63750c1477e755ce2391e28b
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-04-01 21:47:08 +0300

    Update hentai.js

--- commands/nsfw/hentai.js
+++ commands/nsfw/hentai.js
@@ -10,7 +10,7 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
 
                 .setAuthor(`- Hentai`, message.author.displayAvatarURL({ dynamic: true }))
-                .setImage(`${gif.nsfw.randomHentaiGif(url)}`)
+                .setURL(`${gif.nsfw.randomHentaiGif()}`)
                 .setColor('#7d77df')
             message.channel.send(embed);
         } else {
