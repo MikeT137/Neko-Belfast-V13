7c42d97d7210e820a2513f835249a3da8f6057f7
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-04-01 22:25:42 +0300

    Update hentai.js

--- commands/nsfw/hentai.js
+++ commands/nsfw/hentai.js
@@ -5,12 +5,19 @@ module.exports = {
         const Discord = require('discord.js');
     	const NSFW = require("discord-nsfw");
         const nsfw = new NSFW();
+        const hentai = [
+            nsfw.hentai(),
+            nsfw.hentaiass(),
+            nsfw.hmidriff(),
+            nsfw.hentaithigh()
+        ]
+        const image = hentai[Math.floor(Math.random() * hentai.length)];
 
         if (message.channel.nsfw) {
             const embed = new Discord.MessageEmbed()
 
                 .setAuthor(`- Hentai`, message.author.displayAvatarURL({ dynamic: true }))
-                .setURL(`${nsfw.hentai()}`)
+                .setURL(`${image}`)
                 .setColor('#7d77df')
             message.channel.send(embed);
         } else {
