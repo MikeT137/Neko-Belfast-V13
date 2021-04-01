63d56b70b221d63e607ce72756370e6752d87b71
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-04-01 22:10:48 +0300

    vry

--- commands/nsfw/hentai.js
+++ commands/nsfw/hentai.js
@@ -4,13 +4,13 @@ module.exports = {
     run: async (bot, message, args) => {
         const Discord = require('discord.js');
     	const neko = require('nekos.life');
-    	const gif = new neko();
+    	const {nsfw} = new neko();
 
         if (message.channel.nsfw) {
             const embed = new Discord.MessageEmbed()
 
                 .setAuthor(`- Hentai`, message.author.displayAvatarURL({ dynamic: true }))
-                .setURL(`${gif.nsfw.randomHentaiGif()}`)
+                .setURL(`${nsfw.randomHentaiGif()}`)
                 .setColor('#7d77df')
             message.channel.send(embed);
         } else {
