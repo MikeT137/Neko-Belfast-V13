6509b12da6401a94672b49d1795b77eed9a56745
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-04-02 21:39:53 +0300

    Update storyrun.js

--- commands/fun/storyrun.js
+++ commands/fun/storyrun.js
@@ -269,26 +269,21 @@ module.exports = {
                                         if(reaction13.emoji.name === '👍' || reaction13.emoji.name === '👎') {
                                             message.channel.stopTyping();
                                             const choice = [
-                                                msg.edit(`*You were correct: alright then, i will help you into killing the siren head! You then make a plan, and you, the hunter and Edward, start going after siren head: target spoted, charge on!! You throw your axe at siren head\'s chest, then the hunter shots siren head with his shotgun shells, then Edward gets his sword and...after all this trouble, siren head was defeated.*\n\n**YOU SURVIVED**\nEnding 16 out of ${endings}: The three musketeers\n${artist}`)
+                                                (`*You were correct: alright then, i will help you into killing the siren head! You then make a plan, and you, the hunter and Edward, start going after siren head: target spoted, charge on!! You throw your axe at siren head\'s chest, then the hunter shots siren head with his shotgun shells, then Edward gets his sword and...after all this trouble, siren head was defeated.*\n\n**YOU SURVIVED**\nEnding 16 out of ${endings}: The three musketeers\n${artist}`)
                                                 .then(m => {
                                                     setTimeout(function(){
                                                         m.edit('**YOU SURVIVED**')
                                                     }, 45000)
-                                                })
-                                            ]
-                                            const choice2 = [
-                                                msg.edit(`*You were wrong. You try to run away but then Edward gets his sword out with his left hand and slices your head off*\n\n**YOU DIED**\nEnding 15 out of ${endings}: The lost bet\n${artist}`)
+                                                }),
+
+                                                (`*You were wrong. You try to run away but then Edward gets his sword out with his left hand and slices your head off*\n\n**YOU DIED**\nEnding 15 out of ${endings}: The lost bet\n${artist}`)
                                                 .then(n => {
                                                     setTimeout(function(){
                                                         n.edit('**YOU DIED**')
                                                     }, 45000)
                                                 })
                                             ]
-                                            let choices = [
-                                                choice,
-                                                choice2
-                                            ]
-                                            choices[Math.floor(Math.random() * choices.length)];
+                                            msg.edit(choice[Math.floor(Math.random() * choice.length)])
                                         }
                                         if(reaction13.emoji.name === '✊') {
                                             message.channel.stopTyping();
