29ec2eadcc457e8e0f069a2d145d688e89eb18af
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-04-05 12:34:02 +0300

    Update help.js

--- commands/moderation/help.js
+++ commands/moderation/help.js
@@ -281,11 +281,7 @@ module.exports = {
                 message.channel.send(embedS2);
             break;
 
-            case 'blush' || 'bored' || 'clap' || 'confused' || 'cry' || 'dab' || 'dance' || 'drool' || 'facepalm' || 'howl' || 'laugh' || 'nom' || 'peek' || 'pout' || 'purr' || 'rage' || 'run' || 'scared' || 'shrug' || 'sigh':
-                message.channel.send(`It sends a ${args[0]}ing gif: \`b.${args[0]}\``)
-            break;
-
-            case 'sip' || 'smile' || 'smug' || 'surprised' || 'think' || 'wag' || 'wink' || 'yawn':
+            case 'blush' || 'bored' || 'clap' || 'confused' || 'cry' || 'dab' || 'dance' || 'drool' || 'facepalm' || 'howl' || 'laugh' || 'nom' || 'peek' || 'pout' || 'purr' || 'rage' || 'run' || 'scared' || 'shrug' || 'sigh' || 'sip' || 'smile' || 'smug' || 'surprised' || 'think' || 'wag' || 'wink' || 'yawn':
                 message.channel.send(`It sends a ${args[0]}ing gif: \`b.${args[0]}\``)
             break;
         }
