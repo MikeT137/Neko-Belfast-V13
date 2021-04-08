9d76c0a82c57a126de461989489e4f736272c793
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-04-08 19:43:51 +0300

    asdf

---
+++ commands/moderation/enlarge.js
@@ -0,0 +1,36 @@
+const {prefix} = require('../../config.json')
+module.exports = {
+    name:'henlarge',
+    description: "It sends a bigger version of an emoji",
+    usage: `${prefix}enlarge (emoji)`,
+    category: 'moderation',
+    run: async (bot, message, args) => {
+        const emoji = args[0];
+        const custom = Discord.Util.parseEmoji(emoji);
+
+        if (!emoji) {
+            message.channel.send("No emoji provided!");
+        }
+        if (custom.id) {
+            const embed = new Discord.MessageEmbed()
+        
+                .setAuthor(`Enlarged version of ${emoji}`, message.author.displayAvatarURL({ dynamic: true }))
+                .setColor('#7d77df')
+                .setImage(`https://cdn.discordapp.com/emojis/${custom.id}.${custom.animated ? "gif" : "png"}`);
+            message.channel.send(embed);
+        }
+        else {
+            let parsed = parse(emoji, { assetType: "png" });
+
+            if (!parsed[0]) {
+                message.channel.send("Invalid emoji!");
+            }else {
+                const embed = new Discord.MessageEmbed()
+
+                    .setColor('#7d77df')
+                    .setImage(parse[0].url);
+                message.channel.send(embed);
+            }
+        }
+    }
+}
--- commands/moderation/help.js
+++ commands/moderation/help.js
@@ -11,7 +11,7 @@ module.exports = {
         const fun = '\`blackjack\`, \`cookie\`, \`holiday\`, \`math\`, \`meme\`, \`rps\`, \`storyrun\`';
         const info = '\`avatar\`, \`botinfo\`, \`serverinfo\`, \`userinfo\`';
         const interactive = '\`apologise\`, \`bite\`, \`bonk\`, \`boop\`, \`brofist\`, \`cheer\`, \`cuddle\`, \`handhold\`, \`hug\`, \`kiss\`, \`lick\`, \`nuzzle\`, \`pat\`, \`poke\`, \`punch\`, \`shoot\`, \`slap\`, \`snuggle\`, \`stare\`, \`tickle\`, \`wave\`';
-        const mod = '\`ban\`, \`clear\`, \`help\`, \`kick\`, \`mute\`, \`ping\`, \`suggest\`, \`unban\`, \`unmute\`';
+        const mod = '\`ban\`, \`clear\`, \`enlarge\`, \`help\`, \`kick\`, \`mute\`, \`ping\`, \`suggest\`, \`unban\`, \`unmute\`';
         const random = '\`8ball\`, \`coinflip\`, \`rng\`, \`ship\`';
         const self = '\`blush\`, \`bored\`, \`clap\`, \`confused\`, \`cry\`, \`dab\`, \`dance\`, \`drool\`, \`facepalm\`, \`howl\`, \`laugh\`, \`nom\`, \`peek\`, \`pout\`, \`purr\`, \`rage\`, \`run\`, \`scared\`, \`shrug\`, \`sigh\`, \`sip\`, \`smile\`, \`smug\`, \`surprised\`, \`think\`, \`wag\`, \`wink\`, \`yawn\`';
 
