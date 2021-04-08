ce69aaff85b117cc1af23a11ed2efda62f59569e
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-04-08 12:34:16 +0300

    Update holiday.js

--- commands/fun/holiday.js
+++ commands/fun/holiday.js
@@ -91,6 +91,7 @@ module.exports = {
             'https://cdn62.picsart.com/188752053001202.gif?to=min&r=640',
             'https://data.whicdn.com/images/264113355/original.gif'
         ]
+        message.delete();
         const embed = new Discord.MessageEmbed()
 
             .setAuthor(`Happy Easter nya~!`, message.author.displayAvatarURL({ dynamic: true }))
