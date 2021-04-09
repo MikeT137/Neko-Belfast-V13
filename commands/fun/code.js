a5fdf4d551874dc10f140e5cc91e4561adfa2eba
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-04-09 22:04:17 +0300

    Update code.js

--- commands/fun/code.js
+++ commands/fun/code.js
@@ -1,16 +1,7 @@
-const {prefix} = require('../../config.json')
-module.exports = {
-    name: 'code',
-    description: 'bruh',
-    usage: `${prefix}code`,
-    category: 'fun',
-    run: async (bot, message, args) => {
-        function codeGen(bot) {
-            let randomcode = Math.floor(Math.random(111111111)*999999999)
+function codeGen(bot) {
+    let randomcode = Math.floor(Math.random(111111111)*999999999)
 
             console.log(randomcode)
         }
         setInterval(codeGen, 900000)
-            module.exports = { codeGen } 
-    }
-}
+            module.exports = { codeGen }
