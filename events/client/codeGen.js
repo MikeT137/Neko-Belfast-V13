a4cecac893d25d14d966b66261fca95a4c1ebffd
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-04-09 22:12:05 +0300

    Delete codeGen.js

--- events/client/codeGen.js
+++
@@ -1,8 +0,0 @@
-function codeGen(bot) {
-
-    let randomcode = Math.floor(Math.random(111111111)*999999999)
-    console.log(randomcode)
-
-}setInterval(codeGen, 900000)
-
-module.exports = { codeGen }
