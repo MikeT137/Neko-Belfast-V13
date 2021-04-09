e2ad00bb76e5b5c4b9d4d723a38f95c929eb8b29
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-04-09 21:58:51 +0300

    Create code.js

---
+++ commands/fun/code.js
@@ -0,0 +1,16 @@
+const {prefix} = require('../../config.json')
+module.exports = {
+    name: 'code',
+    description: 'bruh',
+    usage: `${prefix}code`,
+    category: 'fun',
+    run: async (bot, message, args) => {
+        function codeGen(bot) {
+            let randomcode = Math.floor(Math.random(111111111)*999999999)
+
+            console.log(randomcode)
+        }
+        setInterval(codegen, 900000)
+            module.exports = { codeGen } 
+    }
+}
