eca4b6826e539aecae03f6fa98ce4a046e096729
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-04-12 11:54:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ bot.aliases = new Collection();
 bot.categories = fs.readdirSync("./commands/");
 
 ["command", "event"].forEach(handler => {
-    require(`./handlers/${handler}`)(bot);
-});
+    require(`./handlers/${handler}`)(bot)
+})
 
 //Discord Bot List - Server Count
 const DBL = require("dblapi.js");
