247eb7752871d68f0c3a43851f5f8bbc331c8d52
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-04-12 12:36:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ bot.categories = fs.readdirSync("./commands/");
 })
 
 //Discord Bot List - Server Count
-const DBL = require("dblapi.js");
+const DBL = require("dblapi.js")
 const topgglink = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcyNzA5MzIzNjk1NDQzMTQ4OCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjAxMDIxNzQ2fQ.QCfXPZhovBGFszhJAtrdBTChuh92-xrtGRoGiCJnYnU';
-const dbl = new DBL(`${topgglink}`, bot);
+const dbl = new DBL(`${topgglink}`, bot)
 
 dbl.on('posted', () => {
-    console.log('Server count posted!');
+    console.log('Server count posted!')
 })
 
 dbl.on('error', e => {
-    console.log(`Oops! ${e}`);
-});
+    console.log(`Oops! ${e}`)
+})
 
 //Token
 bot.login(process.env.token);
