46f1111daea1497e3e65cf4071085cf014c1de82
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-04-30 20:34:34 +0300

    Update meme.js

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -1,13 +1,13 @@
 const {prefix} = require('../../config.json')
 module.exports = {
     name:'meme',
-    description: "It sends a meme off of a random subreddit, those being: dankmemes, memes, meme, Animemes, goodanimemes",
+    description: "It sends a meme off of a random subreddit, those being: dankmemes, memes, meme, Animemes",
     usage: `${prefix}meme`,
     category: 'fun',
     run: async (bot, message, args) => {
         const Discord = require('discord.js')
         const api = require('imageapi.js')
-        const subreddits = ["dankmemes", "goodanimemes", "memes", 'Animemes', "meme"]
+        const subreddits = ["dankmemes", "memes", 'Animemes', "meme"]
         const subreddit = subreddits[Math.floor(Math.random() * (subreddits.length))]
         const meme = await api.advanced(subreddit)
         
