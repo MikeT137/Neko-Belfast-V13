311cbbee3f62b00cf7366f50b33c590da9c03d8b
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-05-01 23:17:02 +0300

    Update owofy.js

--- commands/fun/owofy.js
+++ commands/fun/owofy.js
@@ -5,15 +5,23 @@ module.exports = {
     usage: `${prefix}owofy (message)`,
     category: 'fun',
     run: async (bot, message, args) => {
+        const Discord = require('discord.js')
+        const nekoclient = require('nekos.life');
+        const neko = new nekoclient();
+
         if(!args[0]) {
             message.channel.send('You need to type something to owofy it')
         }else {
-            const nekoclient = require('nekos.life');
-            const neko = new nekoclient();
-
             async function owofy() {
+                message.delete();
+
                 const owoTEXT = await neko.sfw.OwOify({text: message.content.split(' ').slice(1).join(' ')});
-                message.channel.send(owoTEXT.owo);
+                const embed = new Discord.MessageEmbed()
+        
+                    .setAuthor(`${message.author.username} says:`, message.author.displayAvatarURL({ dynamic: true }))
+                    .setDescription(`${owoTEXT.owo}`)
+                    .setColor('#7d77df')
+                message.channel.send(embed)
             }
             owofy();
         }
