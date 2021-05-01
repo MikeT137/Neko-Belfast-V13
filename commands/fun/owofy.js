5bdb00461276da8bbd9ab459160302eedc288cf5
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-05-01 22:49:54 +0300

    Update owofy.js

--- commands/fun/owofy.js
+++ commands/fun/owofy.js
@@ -2,16 +2,20 @@ const {prefix} = require('../../config.json')
 module.exports = {
     name:'owofy',
     description: "It sends your exact message, but in the form of \"owo\"",
-    usage: `${prefix}owofy`,
+    usage: `${prefix}owofy (message)`,
     category: 'fun',
     run: async (bot, message, args) => {
-        const nekoclient = require('nekos.life');
-        const neko = new nekoclient();
+        if(!args[0]) {
+            message.channel.send('You need to type something to owofy it')
+        }else {
+            const nekoclient = require('nekos.life');
+            const neko = new nekoclient();
 
-        async function owofy() {
-            const owoTEXT = await neko.sfw.OwOify({text: message.content.split(' ').slice(1).join(' ')});
-            message.channel.send(owoTEXT.owo);
+            async function owofy() {
+                const owoTEXT = await neko.sfw.OwOify({text: message.content.split(' ').slice(1).join(' ')});
+                message.channel.send(owoTEXT.owo);
+            }
+            owofy();
         }
-        owofy();
     }
 }
