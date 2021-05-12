6e5896597b87f13498b4241085ddef739809f08b
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-05-12 13:08:21 +0300

    Update server.js

--- commands/info/server.js
+++ commands/info/server.js
@@ -34,7 +34,6 @@ module.exports = {
             .addField('General', [
                 `\`Name:\` ${message.guild.name}`,
                 `\`ID:\` ${message.guild.id}`,
-                `\`Owner:\` ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
                 `\`Region:\` ${regions[message.guild.region]}`,
                 `\`Boost Tier:\` ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}`: 'None'}`,
                 `\`Time Created:\` ${moment(message.guild.createdTimestamp).format('LT')}; ${moment(message.guild.createdTimestamp).format('LL')}; ${moment(message.guild.createdTimestamp).fromNow()}`
