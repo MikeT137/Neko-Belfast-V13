e6bdcf2d54dd2fc5737a2c0864d794d4d8e59b4f
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-05-16 18:46:23 +0300

    Update rps.js

--- commands/fun/rps.js
+++ commands/fun/rps.js
@@ -9,6 +9,18 @@ module.exports = {
         const acceptedReplies = ['rock', 'paper', 'scissors'];
         const result = acceptedReplies[Math.floor(Math.random() * acceptedReplies.length)];
 
+        const embedR = new Discord.MessageEmbed().setAuthor(`I won!`, message.author.displayAvatarURL({ dynamic: true })).addField('Bot vs You', ':page_facing_up: vs :rock:').setColor('RED')
+        const embedR2 = new Discord.MessageEmbed().setAuthor(`It\'s a tie!`, message.author.displayAvatarURL({ dynamic: true })).addField('Bot vs You', ':rock: vs :rock:').setColor('YELLOW')
+        const embedR3 = new Discord.MessageEmbed().setAuthor(`You won!`, message.author.displayAvatarURL({ dynamic: true })).addField('Bot vs You', ':scissors: vs :rock:').setColor('GREEN')
+
+        const embedP = new Discord.MessageEmbed().setAuthor(`I won!`, message.author.displayAvatarURL({ dynamic: true })).addField('Bot vs You', ':scissors: vs :page_facing_up:').setColor('RED')
+        const embedP2 = new Discord.MessageEmbed().setAuthor(`It's a tie!`, message.author.displayAvatarURL({ dynamic: true })).addField('Bot vs You', ':page_facing_up: vs :page_facing_up:').setColor('YELLOW')
+        const embedP3 = new Discord.MessageEmbed().setAuthor(`You won!`, message.author.displayAvatarURL({ dynamic: true })).addField('Bot vs You', ':rock: vs :page_facing_up:').setColor('GREEN')
+
+        const embedS = new Discord.MessageEmbed().setAuthor(`I won!`, message.author.displayAvatarURL({ dynamic: true })).addField('Bot vs You', ':rock: vs :scissors:').setColor('RED')
+        const embedS2 = new Discord.MessageEmbed().setAuthor(`It\'s a tie!`, message.author.displayAvatarURL({ dynamic: true })).addField('Bot vs You', ':scissors: vs :scissors:').setColor('YELLOW')
+        const embedS3 = new Discord.MessageEmbed().setAuthor(`You won!`, message.author.displayAvatarURL({ dynamic: true })).addField('Bot vs You', ':page_facing_up: vs :scissors:').setColor('GREEN')
+
         if (!args[0]) {
             const embed = new Discord.MessageEmbed()
         
@@ -29,107 +41,62 @@ module.exports = {
                     const reaction = collected.first();
                     if(reaction.emoji.name === '🪨') {
                         if (result === 'paper') {
-                            const embedR = new Discord.MessageEmbed()
-        
-                                .setAuthor(`I won!`, message.author.displayAvatarURL({ dynamic: true }))
-                                .addField('Bot vs You', ':page_facing_up: vs :rock:')
-                                .setColor('RED')
                             msg.edit(embedR)
                         }else if (result === 'rock') {
-                            const embedR2 = new Discord.MessageEmbed()
-        
-                                .setAuthor(`It\'s a tie!`, message.author.displayAvatarURL({ dynamic: true }))
-                                .addField('Bot vs You', ':rock: vs :rock:')
-                                .setColor('YELLOW')
                             msg.edit(embedR2)
                         }else {
-                            const embedR3 = new Discord.MessageEmbed()
-        
-                                .setAuthor(`You won!`, message.author.displayAvatarURL({ dynamic: true }))
-                                .addField('Bot vs You', ':scissors: vs :rock:')
-                                .setColor('GREEN')
                             msg.edit(embedR3)
                         }
                     }else if(reaction.emoji.name === '📄') {
                         if (result === 'scissors') {
-                            const embedP = new Discord.MessageEmbed()
-        
-                                .setAuthor(`I won!`, message.author.displayAvatarURL({ dynamic: true }))
-                                .addField('Bot vs You', ':scissors: vs :page_facing_up:')
-                                .setColor('RED')
                             msg.edit(embedP)
                         }else if (result === 'paper') {
-                            const embedP2 = new Discord.MessageEmbed()
-        
-                                .setAuthor(`It's a tie!`, message.author.displayAvatarURL({ dynamic: true }))
-                                .addField('Bot vs You', ':page_facing_up: vs :page_facing_up:')
-                                .setColor('YELLOW')
                             msg.edit(embedP2)
                         }else {
-                            const embedP3 = new Discord.MessageEmbed()
-        
-                                .setAuthor(`You won!`, message.author.displayAvatarURL({ dynamic: true }))
-                                .addField('Bot vs You', ':rock: vs :page_facing_up:')
-                                .setColor('GREEN')
                             msg.edit(embedP3) 
                         }   
                     }else if(reaction.emoji.name === '✂️') {
                         if (result === 'rock') {
-                            const embedS = new Discord.MessageEmbed()
-        
-                                .setAuthor(`I won!`, message.author.displayAvatarURL({ dynamic: true }))
-                                .addField('Bot vs You', ':rock: vs :scissors:')
-                                .setColor('RED')
                             msg.edit(embedS)
                         }else if (result === 'scissors') {
-                                const embedS2 = new Discord.MessageEmbed()
-            
-                                .setAuthor(`It\'s a tie!`, message.author.displayAvatarURL({ dynamic: true }))
-                                .addField('Bot vs You', ':scissors: vs :scissors:')
-                                .setColor('YELLOW')
                             msg.edit(embedS2)
                         }else {
-                            const embedS3 = new Discord.MessageEmbed()
-        
-                                .setAuthor(`You won!`, message.author.displayAvatarURL({ dynamic: true }))
-                                .addField('Bot vs You', ':page_facing_up: vs :scissors:')
-                                .setColor('GREEN')
                             msg.edit(embedS3)
                         }
                     }
                 })
             })
         }else if (!acceptedReplies.includes(args[0])) {
-            message.channel.send(`You can only use: \`${acceptedReplies.join(', ')}\``);
+            message.channel.send(`You can only use: \`${acceptedReplies.join(', ')}\`, or nothing`);
         }
         switch (args[0]) {
             case 'rock':
                 if (result === 'paper') {
-                    message.channel.send(`I played ${result}. I won!`);
+                    message.channel.send(embedR);
                 }else if (result === args[0]) {
-                    message.channel.send(`I played ${result}. It's a tie!`);
+                    message.channel.send(embedR2);
                 }else {
-                    message.channel.send(`I played ${result}. You won!`);
+                    message.channel.send(embedR3);
                 }
             break;
             
             case 'paper':
                 if (result === 'scissors') {
-                    message.channel.send(`I played ${result}. I won!`);
+                    message.channel.send(embedP);
                 }else if (result === args[0]) {
-                    message.channel.send(`I played ${result}. It's a tie!`);
+                    message.channel.send(embedP2);
                 }else {
-                    message.channel.send(`I played ${result}. You won!`);  
+                    message.channel.send(embedP3);  
                 }      
             break;
 
             case 'scissors':
                 if (result === 'rock') {
-                    message.channel.send(`I played ${result}. I won!`);
+                    message.channel.send(embedS);
                 }else if (result === args[0]) {
-                    message.channel.send(`I played ${result}. It's a tie!`);
+                    message.channel.send(embedS2);
                 }else {
-                    message.channel.send(`I played ${result}. You won!`);
+                    message.channel.send(embedS3);
                 }
             break;
         }
