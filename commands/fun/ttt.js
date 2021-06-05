1f87daa3e7e64803298dcd58703baecdffad35aa
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-06-05 22:10:47 +0300

    tic tac toe

---
+++ commands/fun/ttt.js
@@ -0,0 +1,20 @@
+const {prefix} = require('../../config.json')
+module.exports = {
+    name: 'ttt',
+    description: 'You play tic-tac-toe with another user! The way it works is that you have to react with an emoji to where you want to put your X, and the first one to get a straight line of 3 (X or O) wins! Here\'s the template:\n:one::two::three:\n:four::five::six:\n:seven::eight::nine:',
+    usage: `${prefix}ttt @ping`,
+    category: 'fun',
+    run: async (bot, message, args) => {
+        const person = message.mentions.members.first()
+        const { tictactoe } = require('reconlx')
+
+        if(!args[0]) {
+            message.channel.send('Please specify a member!')
+        }else if(message.mentions.members.size >= 1) {
+            new tictactoe({
+                player_two: person,
+                message: message
+            })
+        }
+    }
+}
