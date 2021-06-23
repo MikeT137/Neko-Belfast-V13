1ed7d2f0accb3d00371d7ce5403e277ecf8d1a04
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-06-23 20:26:23 +0300

    sdf

---
+++ commands/nsfw/futa.js
@@ -0,0 +1,39 @@
+const {prefix} = require('../../config.json')
+const {topggtoken} = require('../../config.json')
+module.exports = {
+    name:'futa',
+    description: "It sends a nsfw gif about futas",
+    usage: `${prefix}futa`,
+    category: 'nsfw',
+    run: async (bot, message, args) => {
+        if(message.channel.nsfw) {
+            const Discord = require('discord.js')
+            const Topgg = require('@top-gg/sdk')
+            const topgg = new Topgg.Api(topggtoken)
+            const reddit = require('reddit-fetch')
+            const subreddits = ["FutanariHentai", "futanari"]
+            const subreddit = subreddits[Math.floor(Math.random() * (subreddits.length))]
+            
+            let voted = await topgg.hasVoted(message.author.id)
+            if(!voted) {
+                message.channel.send(`If you want to access nsfw commands, you have to vote for me once every 12 hours. That\'s the only way that it can work nya~.\nHere\'s the link: https://top.gg/bot/727093236954431488/vote`)
+             }else {
+                reddit({
+                    subreddit: `${subreddit}`,
+                    sort: 'hot',
+                    allowNSFW: true
+                }).then(post => {
+                    const embed = new Discord.MessageEmbed()
+
+                        .setTitle(post.title)
+                        .setImage(post.url)
+                        .setFooter(`Posted in r/${subreddit}`, 'https://cdn.discordapp.com/emojis/697937639701086268.png?v=1')
+                        .setColor('#7d77df')
+                    message.channel.send(embed)
+                }).catch(error => console.log(`Oops, something went wrong:\n${error}`))
+            }
+        }else {
+            message.channel.send(`${message.author.username} what are you doing?! This is not a nsfw channel nyaa!`)
+        }
+    }
+}
---
+++ commands/nsfw/tomboy.js
@@ -0,0 +1,37 @@
+const {prefix} = require('../../config.json')
+const {topggtoken} = require('../../config.json')
+module.exports = {
+    name:'tomboy',
+    description: "It sends a nsfw gif about tomboys",
+    usage: `${prefix}tomboy`,
+    category: 'nsfw',
+    run: async (bot, message, args) => {
+        if(message.channel.nsfw) {
+            const Discord = require('discord.js')
+            const Topgg = require('@top-gg/sdk')
+            const topgg = new Topgg.Api(topggtoken)
+            const reddit = require('reddit-fetch')
+            
+            let voted = await topgg.hasVoted(message.author.id)
+            if(!voted) {
+                message.channel.send(`If you want to access nsfw commands, you have to vote for me once every 12 hours. That\'s the only way that it can work nya~.\nHere\'s the link: https://top.gg/bot/727093236954431488/vote`)
+             }else {
+                reddit({
+                    subreddit: "Tomboy_Hentai",
+                    sort: 'hot',
+                    allowNSFW: true
+                }).then(post => {
+                    const embed = new Discord.MessageEmbed()
+
+                        .setTitle(post.title)
+                        .setImage(post.url)
+                        .setFooter(`Posted in r/Tomboy_Hentai`, 'https://cdn.discordapp.com/emojis/697937639701086268.png?v=1')
+                        .setColor('#7d77df')
+                    message.channel.send(embed)
+                }).catch(error => console.log(`Oops, something went wrong:\n${error}`))
+            }
+        }else {
+            message.channel.send(`${message.author.username} what are you doing?! This is not a nsfw channel nyaa!`)
+        }
+    }
+}
