5fc314faf1682ffa72d7c17c4bf479a01d55fbba
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-07-02 19:49:08 +0300

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,6 +1,6 @@
 const {prefix} = require('../../config.json')
 module.exports = bot => {
-    console.log('Neko Belfast is online!');
+    console.log(`${bot.user.username} is online!`);
     bot.user.setActivity(`with my tail nya~ | ${prefix}help`, {type: 'PLAYING'}).catch(console.error);
 
     /*
