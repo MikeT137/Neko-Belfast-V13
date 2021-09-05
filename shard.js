56f6fac8f49b7a731fbb9c4f59e216685387b67f
Ian Woo <archerymaster12354@gmail.com>
2021-09-05 05:47:20 +0000

    fixing package.json

--- shard.js
+++ shard.js
@@ -1,7 +1,7 @@
 const { ShardingManager } = require("discord.js")
 
 const shards = new ShardingManager("./index.js", {
-    token: process.env.token, 
+    token: "NzI3MDkzMjM2OTU0NDMxNDg4.Xvm0WA.SKa92unlj_4RZvYAy2Tf6ZiwvJ8", 
     totalShards: "auto"
 })
 
