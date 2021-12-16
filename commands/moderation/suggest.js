bc510ebdb70fa059308b2ffacae1cb1b2291ffab
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2021-12-16 16:53:34 +0200

    pog

--- commands/moderation/suggest.js
+++ commands/moderation/suggest.js
@@ -11,27 +11,41 @@ module.exports = {
         if(!args[0]) {
             message.channel.send('You have to say something to suggest it to the creator, but please try to be as explicit as possible.')
         }else if(args[0]) {
-            bot.users.fetch(`${mikuid}`).then((user) => {
-                const embed = new Discord.MessageEmbed()
+            bot.users.fetch(`${mikuid}`).then((miku) => {
+                message.channel.send('Your suggestion has been sent to the creator! Please wait for the creator to respond! (This message will be edited once he responds)').then(suggestion => {
+                    const embed = new Discord.MessageEmbed()
 
-                    .setDescription('**New Suggestion nya~!**')
-                    .setThumbnail(message.author.displayAvatarURL({dynamic: true}))
-                    .addField('User', [
-                        `\`User Tag:\` ${message.author.tag}`,
-                        `\`User ID:\` ${message.author.id}`,
-                    ])
-                    .addField(`Server`, [
-                        `\`Server Name:\` ${message.guild.name}`,
-                        `\`Server ID\`: ${message.guild.id}`,
-                    ])
-                    .addField('Message', `${args.join(' ')}`)
-                    .setColor('#7d77df')
-                user.send(embed).then(msg => {
-                    msg.react('✅'),
-                    msg.react('❌')
+                        .setDescription('**New Suggestion nya~!**')
+                        .setThumbnail(message.author.displayAvatarURL({dynamic: true}))
+                        .addField('User', [
+                            `\`User Tag:\` ${message.author.tag}`,
+                            `\`User ID:\` ${message.author.id}`,
+                        ])
+                        .addField(`Server`, [
+                            `\`Server Name:\` ${message.guild.name}`,
+                            `\`Server ID\`: ${message.guild.id}`,
+                        ])
+                        .addField('Message', `${args.join(' ')}`)
+                        .setColor('#7d77df')
+                    miku.send(embed).then(msg => {
+                        msg.react('✅'),
+                        msg.react('❌')
+
+                        const filter = (reaction, user) => {
+                            return ['✅', '❌'].includes(reaction.emoji.name) && user.id === mikuid;
+                        }
+
+                        msg.awaitReactions(filter, { max: 1 }).then(collected => {
+                            const reaction = collected.first();
+
+                            if(reaction.emoji.name === '✅') {
+                                suggestion.edit(':white_check_mark: - The creator has approved your suggestion and he will work or is working on it soon, thank you!')
+                            }else if(reaction.emoji.name === '❌') {
+                                suggestion.edit(':x: - The creator has rejected your suggestion, sorry!')
+                            }
+                        })
+                    }).catch(error => console.log(`Oops, something went wrong:\n${error}`))
                 })
-
-                message.channel.send('Your suggestion has been sent to the creator, thank you for your time<3')
             }).catch(error => console.log(`Oops, something went wrong:\n${error}`))
         }else return;
     }
