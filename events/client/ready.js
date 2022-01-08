d9e79943f2e8d136bd39d77dce09484e7efb2a20
MikeT137 <67682962+MikeT137@users.noreply.github.com>
2022-01-08 11:12:06 +0200

    no more holidays :(

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,21 +1,5 @@
 const {prefix} = require('../../config.json')
 module.exports = bot => {
     console.log(`${bot.user.username} is online!`);
-    bot.user.setActivity(`Merry Xmas nya! | ${prefix}help`, {type: 'PLAYING'}).catch(console.error);
-
-    /*
-    Holidays
-
-    January 1st - Happy New Year nya~!
-    February 14th - Will you be my Valentine?
-    April 8th - Happy Easter nya~!
-    June 29th - It\'s my birthday nya~!
-    September 16th - Happy birthday Miku!
-    October 31st - Have a spooky Halloween tee hee
-    November 25th - Happy Thanksgiving nya~!
-    November 30th - It\'s Mono\'s birthday nya~!
-    December 25th - Merry Xmas nya!
-
-    Every other day - with my tail nya~
-    */
+    bot.user.setActivity(`with my tail nya~ | ${prefix}help`, {type: 'PLAYING'}).catch(console.error);
 }
